7aa4cbc4066ed220b6f692989e0502304bbb5141
Oliv5 <oliv5@caramail.com>
2013-11-20 10:10:54 +0100

    [search] Try to fix set -f issue when cancelling _ff or _gg

--- .profile.d/03_search.sh
+++ .profile.d/03_search.sh
@@ -6,18 +6,22 @@ GREP_EXCLUDE="\.git\|\.svn"
 alias ff='_ff'
 alias iff='NAME=iname _ff'
 function _ff() {
+  trap "set +f; trap SIGINT" SIGINT
   set -f
   NAME=${NAME:-name} find "$(dirname ${1:-.})" \( -$NAME $(sed -e 's/|/ -o -'$NAME' /g' <<< $(basename ${1:-*})) \) -and $FIND_EXCLUDE "${@:2}"
   set +f
+  trap SIGINT
 }
 
 # Search pattern functions
 alias gg='_gg'
 alias igg='NAME=iname _gg'
 function _gg() {
+  trap "set +f; trap SIGINT" SIGINT
   set -f
   NAME=${NAME:-name} _ff "${!#}" -type f -print0 | xargs -0 grep -E -n --color "${@:1:$(($#-1))}"
   set +f
+  trap SIGINT
 }
 
 # Safe search & replace
@@ -25,6 +29,7 @@ alias hh='_hh'
 alias ihh='NAME=iname _hh'
 function _hh()
 {
+  trap "set +f; trap SIGINT" SIGINT
   set -f
   EXCLUDE="$FIND_EXCLUDE -not -type l -and -not -path '*obj*'"
   SEDOPT="${@:1:$(($#-3))}"
@@ -34,6 +39,7 @@ function _hh()
   echo "Press enter or Ctrl-C" ; read
   NAME=${NAME:-name} _ff "${!#}" -type f $EXCLUDE -execdir sed $SEDOPT "s/$IN/$OUT/g" {} \;
   set +f
+  trap SIGINT
 }
 
 # Find and open files functions
