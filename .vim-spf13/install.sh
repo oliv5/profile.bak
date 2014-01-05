a41b2fc49dcbc5aedc2cc45f22d453698c15dd13
Oliv5 <oliv5@caramail.com>
2014-01-06 00:54:51 +0100

    [spf13] Add personal scripts and plugins, MBE missing

--- .vim-spf13/install.sh
+++ .vim-spf13/install.sh
@@ -3,6 +3,9 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 set -x
 rm ~/.vimrc* ~/.vim
 ln -s $DIR/spf13-vim-3/.vim* ~/
-ln -s $DIR/perso/.vim* ~/
-ln -s $DIR/perso/plugin ~/.vim/plugin
+ln -s $DIR/local/.vim* ~/
+mkdir -p ~/.vim/plugin/ 2>/dev/null
+mkdir -p ~/.vim/bundle/ 2>/dev/null
+ln -s $DIR/local/plugin/* ~/.vim/plugin/
+ln -s $DIR/local/bundle/* ~/.vim/bundle/
 set +x
