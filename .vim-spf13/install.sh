3073ea7dc41c499504fbe19ef55e4be855485fa5
Oliv5 <oliv5@caramail.com>
2014-01-11 18:24:17 +0100

    [spf13] Fix install script

--- .vim-spf13/install.sh
+++ .vim-spf13/install.sh
@@ -5,8 +5,8 @@ set -x
 rm ~/.vimrc* ~/.vim
 ln -s $DIR/$SPF13/.vim* ~/
 ln -s $DIR/custom/.vim* ~/
-mkdir -p $DIR/$SPF13/.vim/plugin/ 2>/dev/null
-mkdir -p $DIR/$SPF13/.vim/bundle/ 2>/dev/null
-ln -s $DIR/custom/plugin/* $DIR/$SPF13/.vim/plugin/
+#mkdir -p $DIR/$SPF13/.vim/plugin/ 2>/dev/null
+#mkdir -p $DIR/$SPF13/.vim/bundle/ 2>/dev/null
+ln -s $DIR/custom/plugin $DIR/$SPF13/.vim/plugin
 ln -s $DIR/custom/bundle/* $DIR/$SPF13/.vim/bundle/
 set +x
