e744dd5126e7bfa4e0ef0e0ad72b98f8f00829f8
Oliv5 <oliv5@caramail.com>
2014-07-23 22:12:35 +0200

    [env] Fix cdb & cdf

--- .profile.d/02_cd.sh
+++ .profile.d/02_cd.sh
@@ -10,10 +10,9 @@ g_forward_stack_maxsize=10
 
 # Stack based cd/back functions
 function cda()  { _PWD="$PWD"; builtin cd "$@" && ${PUSH:-true} && push g_backward_stack "$_PWD" && pkeepq g_backward_stack $g_backward_stack_maxsize; }
-function cdb()  { push g_forward_stack "$_PWD" && pkeepq g_forward_stack $g_forward_stack_maxsize; pop g_backward_stack DIR 2>/dev/null && PUSH=false cd "$DIR"; }
-function cdf()  { push g_backward_stack "$_PWD" && pkeepq g_backward_stack $g_backward_stack_maxsize; pop g_forward_stack DIR 2>/dev/null && PUSH=false cd "$DIR"; }
+function cdb()  { pop g_backward_stack DIR 2>/dev/null && push g_forward_stack "$DIR" && pkeepq g_forward_stack $g_forward_stack_maxsize && PUSH=false cd "$DIR"; }
+function cdf()  { pop g_forward_stack DIR 2>/dev/null && push g_backward_stack "$DIR" && pkeepq g_backward_stack $g_backward_stack_maxsize && PUSH=false cd "$DIR"; }
 
 # Alias (do not put this before cdb function definition)
-alias scd='echo "PWD[${#g_backward_stack[@]}]: ${g_backward_stack[@]}"'
+alias scd='echo "backward[${#g_backward_stack[@]}]: ${g_backward_stack[@]}"; echo "forward[${#g_forward_stack[@]}]: ${g_forward_stack[@]}"'
 alias cd='cda'
-
