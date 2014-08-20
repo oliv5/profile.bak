f52513a1bda03c06c54948fe79a8c4643a890060
Oliv5 <oliv5@caramail.com>
2014-08-20 18:53:55 +0200

    [vim] Fix issue in vim-legacy install script

--- .vim-legacy/install.sh
+++ .vim-legacy/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Backup current config
-TODAY=`date +%Y%m%d_%s`
+TODAY=`date +%Y%m%d`
 for VIM in ~/.vim ~/.vimrc*; do
     mv -v $VIM $VIM.$TODAY
 done
 
 # Make links
 SRC=$( cd "$( dirname "${BASH_SOURCE[0]}" )" ; pwd )
-DST=~
-ln -s $SRC/.vim* $DST/ -f
+ln -fsv $SRC/.vim* ~/
+
