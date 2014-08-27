b499bc68b2e21ab0655857d0a6b6890def3253fd
Oliv5 <oliv5@caramail.com>
2014-08-27 15:05:00 +0200

    [vim] Move install.sh backup in dir .vimdata/vimbackup

--- .vim-legacy/install.sh
+++ .vim-legacy/install.sh
@@ -2,9 +2,9 @@
 
 # Backup current config
 TODAY=`date +%Y%m%d`
-mkdir -p ~/.vimbackup
+mkdir -p ~/.vimdata/vimbackup
 for VIM in ~/.vim ~/.vimrc*; do
-    mv -v $VIM ~/.vimbackup/$(basename $VIM).$TODAY.bak
+    mv -v $VIM ~/.vimdata/vimbackup/$(basename $VIM).$TODAY.bak
 done
 
 # Make links
