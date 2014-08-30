fc5c62f71be5651b098adc6a3b8dc7f80dadc33c
Oliv5 <oliv5@caramail.com>
2014-08-31 01:28:28 +0200

    [vim-spf13] Update install script

--- .vim-spf13/install.sh
+++ .vim-spf13/install.sh
@@ -14,6 +14,6 @@ ln -fsv $SRC/.vimrc* ~/
 for BUNDLE in ~/.vim-legacy/.vim/bundle/*; do
     ln -fsv $BUNDLE ~/.vim/bundle/
 done
-#mkdir -p ~/.vim/plugin
+mkdir -p ~/.vim/plugin
 ln -fsv ~/.vim-legacy/.vim/plugin/*.vim ~/.vim/plugin/
 
