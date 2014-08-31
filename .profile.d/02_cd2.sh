d8769a0a7bbd621953dfc8ca08fc58b6c8610806
Oliv5 <oliv5@caramail.com>
2014-08-31 21:21:52 +0200

    [env] Make aliases for pushd and popd to cda and cdb

--- .profile.d/02_cd2.sh
+++ .profile.d/02_cd2.sh
@@ -6,6 +6,10 @@ function cda { builtin cd "$@" && builtin pushd -n "$OLDPWD" >/dev/null 2>&1; }
 function cdb { builtin popd >/dev/null 2>&1 && push g_forward_stack "$OLDPWD" && cd .; }
 function cdf { pop g_forward_stack DIR >/dev/null 2>&1 && cd "$DIR"; }
 
-# Aliases
+# Replace cd, pushd and popd
 alias cd='cda'
+alias pushd='cda'
+alias popd='cdb'
+
+# Display stacks
 alias scd='echo -n "Backward: ";dirs; echo "Forward: $g_forward_stack"'
