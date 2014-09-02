df8dd00f4408164cd902aa177b4cda414ab37d91
Oliv5 <oliv5@caramail.com>
2014-09-02 18:48:53 +0200

    [env] Limit cd forward stack size

--- .profile.d/02_cd2.sh
+++ .profile.d/02_cd2.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 declare -a g_forward_stack=()
+export g_forward_stack_maxsize=10
 
 # Pushd/popd based cd/back functions
 function cda { ${PUSH:-true} && builtin cd "$@" && builtin pushd -n "$OLDPWD" >/dev/null 2>&1; }
-function cdb { builtin popd >/dev/null 2>&1 && push g_forward_stack "$OLDPWD" && PUSH=false cd .; }
+function cdb { builtin popd >/dev/null 2>&1 && push g_forward_stack "$OLDPWD" && pkeepq g_forward_stack $g_forward_stack_maxsize && PUSH=false cd .; }
 function cdf { pop g_forward_stack DIR >/dev/null 2>&1 && cd "$DIR"; }
 
 # Replace cd, pushd and popd
