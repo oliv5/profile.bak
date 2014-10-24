915155e31e513e5917ec0cbd4c0fece05ab2dadf
Oliv5 <oliv5@caramail.com>
2014-10-24 19:07:55 +0200

    [spf13] Disabled legacy bundles by default - skip link creation

--- .vim-spf13/install.sh
+++ .vim-spf13/install.sh
@@ -15,10 +15,10 @@ done
 SRC=$( cd "$( dirname "${BASH_SOURCE[0]}" )" ; pwd )
 ln -fsv ~/.spf13-vim-3/.vim* ~/
 ln -fsv $SRC/.vimrc* ~/
-mkdir -p ~/.vim/bundle
-for BUNDLE in ~/.vim-legacy/.vim/bundle/*; do
-	ln -fsv $BUNDLE ~/.vim/bundle/
-done
+#mkdir -p ~/.vim/bundle
+#for BUNDLE in ~/.vim-legacy/.vim/bundle/*; do
+#	ln -fsv $BUNDLE ~/.vim/bundle/
+#done
 mkdir -p ~/.vim/plugin
 for PLUGIN in ~/.vim-legacy/.vim/plugin/*; do
 	ln -fsv $PLUGIN ~/.vim/plugin/
