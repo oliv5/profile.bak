ac2e01d80ff7ef8c4a6cf01700b0daf3a3c6bd32
Oliv5 <oliv5@caramail.com>
2014-10-25 12:32:36 +0200

    [sourceinsight] Store the right config file (GLOBAL.CF3) + add install & run scripts + update serial key archive

---
+++ .config/sourceinsight/install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+REPO="$HOME/.config/sourceinsight"
+PREFIX="$HOME/.wine-sourceinsight"
+USER_PREFIX="$PREFIX/drive_c/users/$USER"
+EXE="$1"
+
+if ! command -v wine >/dev/null; then
+	echo "Wine not found.Abort..."
+	return 1
+fi
+
+# Init the prefix
+WINEPREFIX="$PREFIX" wine cmd /c echo Installing sourceinsight
+
+# Convert user links to directory in the prefix itself
+find "$USER_PREFIX" -maxdepth 1 -type l -execdir sh -c 'rm "{}"; mkdir "{}"' \;
+
+# Start the install process
+if [ ! -z "$EXE" ]; then
+	WINEPREFIX="$PREFIX" wine "$EXE"
+fi
+
+# Link the configuration file to the repository
+ln -sf "$REPO/GLOBAL.CF3" "$USER_PREFIX/Mes documents/Source Insight/Settings/GLOBAL.CF3"
+
+# Copy run script into user bin
+cp "$REPO/run.sh" "$HOME/bin/sourceinsight.sh"
+
+#WINEPREFIX="$PREFIX" reg add "HKLM\SYSTEM\CurrentControlSet\services\Service" /v "KeyName" /d "Parameters" /f
+#WINEPREFIX="$PREFIX" wine regedit
---
+++ .config/sourceinsight/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+PREFIX="$HOME/.wine-sourceinsight"
+DIR="$PREFIX/drive_c/Program Files (x86)/Source Insight 3"
+if [ ! -d "$DIR" ]; then
+	DIR="$PREFIX/drive_c/Program Files/Source Insight 3"
+fi
+WINEPREFIX="$PREFIX" wine "$DIR/Insight3.exe"
