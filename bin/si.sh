6de1a7100ebff8a9e5eb5610f4a2abb738144010
Oliv5 <oliv5@caramail.com>
2014-10-28 11:59:35 +0100

    [si] move and rename run script into the user bin directory

--- bin/si.sh
+++ bin/si.sh
@@ -4,4 +4,4 @@ DIR="$PREFIX/drive_c/Program Files (x86)/Source Insight 3"
 if [ ! -d "$DIR" ]; then
 	DIR="$PREFIX/drive_c/Program Files/Source Insight 3"
 fi
-WINEPREFIX="$PREFIX" wine "$DIR/Insight3.exe"
+WINEPREFIX="$PREFIX" wine "$DIR/Insight3.exe" &
