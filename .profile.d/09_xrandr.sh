a021e3ae105563b0faf9008ec20acac928dc052a
Oliv5 <oliv5@caramail.com>
2014-12-29 18:46:36 +0100

    [env] add xrandr facilities

--- .profile.d/09_xrandr.sh
+++ .profile.d/09_xrandr.sh
@@ -2,10 +2,21 @@
 
 # Get screen resolution
 function xrandr-getres() {
-  xrandr | grep \* | cut -d' ' -f4
+  xrandr --current | grep \* | cut -d' ' -f4
 }
 
 # Set screen resolution
 function xrandr-setres() {
-  xrandr -s ${1:?Please give screen resolution, like 1280x1024} ${@:2}
+  xrandr -s ${1:?No resolution specified, like 1280x1024} ${@:2}
+}
+
+# Enable display
+function xrandr-on() {
+  #See options like --dryrun, --mode 1024x768
+  xrandr --output "${1:?No display specified}" --auto ${@:2}
+}
+
+# Disable display
+function xrandr-off() {
+  xrandr --output "${1:?No display specified}" --off ${@:2}
 }
