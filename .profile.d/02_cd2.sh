c5ac7e360d6fb30bcdaa3b0a9f04cda16f2be211
Oliv5 <oliv5@caramail.com>
2015-01-03 13:50:14 +0100

    [env] do not use cd alias, which is not resolved by cda/cdb functions

--- .profile.d/02_cd2.sh
+++ .profile.d/02_cd2.sh
@@ -8,11 +8,13 @@ function cdb { builtin popd >/dev/null 2>&1 && push g_forward_stack "$OLDPWD" &&
 function cdf { pop g_forward_stack DIR >/dev/null 2>&1 && cd "$DIR"; }
 
 # Replace cd, pushd and popd
-alias cd='cda'
+function cd() { cda "$@"; }
+unalias cd 2>/dev/null
 
 # Overload pushd and popd functions
 function pushd { builtin pushd "$@" >/dev/null 2>&1; }
 function popd  { builtin popd  "$@" >/dev/null 2>&1; }
+unalias pushd popd 2>/dev/null
 
 # Display stacks
 alias scd='echo -n "Backward: ";dirs; echo "Forward: $g_forward_stack"'
