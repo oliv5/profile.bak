477d1ce50fce20ec96673f786db49c76fdab50dc
Oliv5 <oliv5@caramail.com>
2015-01-07 19:50:43 +0100

    [env] re-attach to existing screen session or create a new one when starting a login interactive shell

--- .profile.d/99_end.sh
+++ .profile.d/99_end.sh
@@ -13,9 +13,19 @@ for i in $HOME/.profile.local.d/*.sh ; do
   fi
 done
 
-# Export user functions
-#fct-export-all
-
 # Cleanup path
 export PATH="${PATH//\~/${HOME}}"
 export PATH="${PATH//.:/}"
+
+# Export user functions
+#fct-export-all
+
+# Screen : re-attach session, or print the list
+# see http://www.saltycrane.com/blog/2008/01/how-to-scroll-in-gnu-screen/
+# [[ $- == *i* ]] && echo 'Interactive' || echo 'Not interactive'
+# shopt -q login_shell && echo 'Login shell' || echo 'Not login shell'
+if [[ $- == *i* ]] && shopt -q login_shell && command -pv screen >/dev/null; then
+  command -p screen -D -R
+  #command -p screen -list
+  #echo "To reattach a session: screen -r <session>"
+fi
