331f89ff1971fa1fc46b495f20c5c320670f1912
Oliv5 <oliv5@caramail.com>
2015-01-09 20:20:33 +0100

    [env] fix screen loading condition, now only once in a session

--- .profile.d/98_screen.sh
+++ .profile.d/98_screen.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+# Backup display
+export XDISPLAY="$DISPLAY"
+
 # Alias
 alias screen-list='screen -ls'
+alias screen-restore='screen -R -D'
 alias screen-killd='screen -ls | grep detached | cut -d. -f1 | awk '\''{print $1}'\'' | xargs -r kill'
 alias screen-killa='screen -ls | grep pts | cut -d. -f1 | awk '\''{print $1}'\'' | xargs -r kill'
 
@@ -9,7 +13,7 @@ alias screen-killa='screen -ls | grep pts | cut -d. -f1 | awk '\''{print $1}'\''
 # see http://www.saltycrane.com/blog/2008/01/how-to-scroll-in-gnu-screen/
 # [[ $- == *i* ]] && echo 'Interactive' || echo 'Not interactive'
 # shopt -q login_shell && echo 'Login shell' || echo 'Not login shell'
-if [[ $- == *i* ]] && shopt -q login_shell && command -pv screen >/dev/null; then
+if [[ -z "$ENV_PROFILE_DONE" ]] && [[ $- == *i* ]] && shopt -q login_shell && command -pv screen >/dev/null; then
   command -p screen -D -R
   #command -p screen -list
   #echo "To reattach a session: screen -r <session>"
--- .profile.d/99_end.sh
+++ .profile.d/99_end.sh
@@ -6,3 +6,7 @@ export PATH="${PATH//.:/}"
 
 # Export user functions
 #fct-export-all
+
+# Set load flag
+export ENV_CNT=$(expr ${ENV_CNT:-0} + 1)
+export ENV_PROFILE_DONE=$ENV_CNT
