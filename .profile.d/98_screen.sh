20751584b8bea97e8f5e16836ea154b0fb502b3d
Oliv5 <oliv5@caramail.com>
2015-01-10 12:42:35 +0100

    [env] change screen/tmux autostart conditions, tmux is now the default

--- .profile.d/98_screen.sh
+++ .profile.d/98_screen.sh
@@ -11,6 +11,6 @@ alias screen-killd='screen -ls | grep detached | cut -d. -f1 | awk '\''{print $1
 alias screen-killa='screen -ls | grep pts | cut -d. -f1 | awk '\''{print $1}'\'' | xargs -r kill'
 
 # Screen : re-attach session, or print the list
-if [[ ! -z "$SCREEN_AUTOLOAD" && -z "$ENV_PROFILE_DONE" && $- == *i* ]] && shopt -q login_shell && command -pv screen >/dev/null; then
-  command -p screen -D -R
+if [[ ! -z "$SCREEN_AUTOLOAD" && -z "$ENV_PROFILE_DONE" && $- == *i* ]] && shopt -q login_shell; then
+  command -pv screen 2>/dev/null && (command -p screen -D -R)
 fi
--- .profile.d/98_tmux.sh
+++ .profile.d/98_tmux.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # Variables
-TMUX_AUTOLOAD=""
+TMUX_AUTOLOAD=1
+TMUX="$(command -pv tmux)"
 
 # Screen : re-attach session, or print the list
-if [[ ! -z "$TMUX_AUTOLOAD" && -z "$ENV_PROFILE_DONE" && $- == *i* ]] && shopt -q login_shell && command -pv tmux >/dev/null; then
-  command -p tmux attach -d
+if [[ ! -z "$TMUX_AUTOLOAD" && -z "$ENV_PROFILE_DONE" && $- == *i* ]] && shopt -q login_shell; then
+  command -pv tmux 2>/dev/null && (command -p tmux attach -d || command -p tmux new-session)
 fi
