6443bc8976f5e92970acef7ee677c6e991d58c34
Oliv5 <oliv5@caramail.com>
2015-01-20 19:16:06 +0100

    [env] fix cda() returning true upon cd error

--- .profile.d/02_cd.sh
+++ .profile.d/02_cd.sh
@@ -11,7 +11,7 @@ fi
 #function cdb_old() { cd "$PWD_0"; }
 
 # Stack based cd/back functions
-function cda()  { builtin cd "$@" && ${ENV_PUSH:-true} || return 0 && push g_backward_stack "$OLDPWD" && pkeepq g_backward_stack $g_backward_stack_maxsize; }
+function cda()  { builtin cd "$@" && (${ENV_PUSH:-true} || return 0) && push g_backward_stack "$OLDPWD" && pkeepq g_backward_stack $g_backward_stack_maxsize; }
 function cdb()  { pop g_backward_stack DIR 2>/dev/null && push g_forward_stack "$PWD" && pkeepq g_forward_stack $g_forward_stack_maxsize && ENV_PUSH=false cd "$DIR"; }
 function cdf()  { pop g_forward_stack DIR 2>/dev/null && push g_backward_stack "$PWD" && pkeepq g_backward_stack $g_backward_stack_maxsize && ENV_PUSH=false cd "$DIR"; }
 
