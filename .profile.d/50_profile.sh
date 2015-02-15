95886173976445f080231964529fcff8e718a216
Oliv5 <oliv5@caramail.com>
2015-02-16 00:51:43 +0100

    [env] update env counter when loading local profiles

--- .profile.d/50_profile.sh
+++ .profile.d/50_profile.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 # Call local profile scripts
 if [ -x ~/.localrc ]; then
+  export ENV_LOCALRC=$((ENV_CNT=ENV_CNT+1))
   source ~/.localrc
 fi
 
 if [ -x ~/.profile.local ]; then
+  export ENV_PROFILE_LOCAL=$((ENV_CNT=ENV_CNT+1))
   source ~/.profile.local
 fi
 
+export ENV_PROFILE_LOCAL_D=$((ENV_CNT=ENV_CNT+1))
 for i in $HOME/.profile.local.d/*.sh ; do
   if [ -x "$i" ]; then
     . "$i"
   fi
 done
+export ENV_PROFILE_LOCAL_D_DONE=$((ENV_CNT=ENV_CNT+1))
