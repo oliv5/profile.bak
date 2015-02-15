46fb80102e8c454b91f8c322046294109bf30051
Oliv5 <oliv5@caramail.com>
2015-02-15 20:45:19 +0100

    [unity] move script into bin/profile, call fct from main

--- bin/profile/unity.sh
+++ bin/profile/unity.sh
@@ -1,27 +1,30 @@
 #!/bin/sh
 
 # http://www.webupd8.org/2012/10/how-to-reset-compiz-and-unity-in-ubuntu.html
-unity-reset() {
-(set -vx
-  sudo apt-get install ccsm
+unity_reset() {
+(
+  local _ANSWER
+  set -vx
   echo "Check ccsm unity plugin is enabled"
   echo "Check the profile is loaded from gconf and is based on the unity template"
-  echo "Press enter"; read
+  echo "Press enter"; read _ANSWER
   ccsm &
   echo "Still doesn't work? reset unity/compiz..."
-  echo "Press enter"; read
-  sudo apt-get install dconf-tools
+  echo "Press enter"; read _ANSWER
   dconf reset -f /org/compiz/
   setsid unity
   echo "Can try compiz --replace and unity --replace"
 )
 }
 
-unity-reseticons() {
+unity_launcher() {
   unity --reset-icons
 }
 
-unity-showconfig() {
-  sudo apt-get install dconf-tools
+unity_showconfig() {
   dconf dump /org/compiz/
 }
+
+# Execute command line when any
+_PREFIX="unity_"
+eval $(echo "$@" | awk 'NF>0 { print "'$_PREFIX'" $0 }')
