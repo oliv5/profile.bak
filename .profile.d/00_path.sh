c0b8a6f55eb92fb11380776712293d2e3666fba5
Oliv5 <oliv5@caramail.com>
2015-02-16 00:48:53 +0100

    [env] move path stuff into its own file

---
+++ .profile.d/00_path.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Prepend to path
+path-prepend() {
+  for DIR in "$@"; do
+    #if ! [[ "$PATH" =~ "${DIR}" ]] && [[ -d "$DIR" ]]; then
+    if [ -d "$DIR" ] && ! (echo "$PATH" | grep "${DIR}" >/dev/null); then
+      export PATH="${DIR}${PATH:+:$PATH}"
+    fi
+  done
+}
+
+# Append to path
+path-append() {
+  for DIR in "$@"; do
+    #if ! [[ "$PATH" =~ "${DIR}" ]] && [[ -d "$DIR" ]]; then
+    if [ -d "$DIR" ] && ! (echo "$PATH" | grep "${DIR}" >/dev/null); then
+      export PATH="${PATH:+$PATH:}${DIR}"
+    fi
+  done
+}
+
+# Cleanup path
+path-cleanup() {
+  #export PATH="${PATH//\~/${HOME}}"
+  #export PATH="${PATH//.:/}"
+  export PATH="$(echo "$PATH" | sed -e 's|~|'"${HOME}"'|g' -e 's|\.\:||g')"
+}
+
+# Main
+unalias path-append 2>/dev/null
+eval path-append /bin /sbin /usr/bin /usr/sbin /usr/local/bin /usr/local/sbin
+unalias path-prepend 2>/dev/null
+eval path-prepend "$HOME/bin" "$HOME/bin/profile"
+
