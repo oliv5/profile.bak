c42561a39dc5e8e9cd169a510b93b6eecdd626f4
Oliv5 <oliv5@caramail.com>
2015-02-16 00:48:20 +0100

    [env] some cleanup

--- .profile.d/05_diff.sh
+++ .profile.d/05_diff.sh
@@ -14,15 +14,16 @@ diffbc() {
 }
 
 diffr() {
-  true ${1:?Please specify input file 1} ${2:?Please specify input file 2} ${3:?Please specify the character range a-b} 
+  true ${1:?Please specify input file 1} ${2:?Please specify input file 2} ${3:?Please specify the character range a-b}
   diff <(cut -b $3 "$1")  <(cut -b $3 "$2")
 }
 
 diffrm() {
-  true ${1:?Please specify input file 1} ${2:?Please specify input file 2} ${3:?Please specify the character range a-b} 
+  true ${1:?Please specify input file 1} ${2:?Please specify input file 2} ${3:?Please specify the character range a-b}
   meld <(cut -b $3 "$1")  <(cut -b $3 "$2")
 }
 
 diffb() {
-  cmp -l ${1:?Please specify input file 1} ${2:?Please specify input file 2} | gawk '{printf "%08X %02X %02X\n", $1-'${3:-0}', strtonum(0$2), strtonum(0$3)}'
+  true ${1:?Please specify input file 1} ${2:?Please specify input file 2}
+  cmp -l "$1" "$2" | gawk '{printf "%08X %02X %02X\n", $1-'${3:-0}', strtonum(0$2), strtonum(0$3)}'
 }
