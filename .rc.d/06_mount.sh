0c270f0a0fc39768f5425728d1495f74fd2dbe0f
Oliv5 <oliv5@caramail.com>
2015-03-26 10:28:29 +0100

    [mount] ecryptfs stop using sudo to chmod the src and dst directories

--- .rc.d/06_mount.sh
+++ .rc.d/06_mount.sh
@@ -11,10 +11,10 @@ mount_ecryptfs() {
   shift $(min 6 $#)
   local OPT="key=passphrase,ecryptfs_enable_filename_crypto=yes,no_sig_cache=yes,ecryptfs_passthrough=no${@:+,$@}"
   OPT="ecryptfs_cipher=$CIPHER,ecryptfs_key_bytes=$KEYLEN,ecryptfs_sig=$KEY1,ecryptfs_fnek_sig=$KEY1,ecryptfs_unlink_sigs${OPT:+,$OPT}"
-  sudo chmod 500 "$SRC"
+  chmod 500 "$SRC"
   sudo ecryptfs-add-passphrase --fnek
   sudo mount -i -t ecryptfs -o "$OPT" "$SRC" "$DST"
-  sudo chmod 700 "$DST"
+  chmod 700 "$DST"
 }
 
 # Mount encfs
