1f7ae0971bd3c0b7add5d7cc9122ca074fea03d0
Oliv5 <oliv5@caramail.com>
2015-04-25 15:43:46 +0200

    [prn] update printer commands

--- .rc.d/20_printer.sh
+++ .rc.d/20_printer.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 # See https://www.cups.org/documentation.php/options.html
+# See https://docs.oracle.com/cd/E23824_01/html/821-1451/gllgm.html
+# Install PPD files in /usr/share/ppd/ -> /etc/cups/ppd
 
 # Printer setup
 prn_setup() {
   sudo lpadmin -p "${1:?Printer name not specified}" -E -v "${2:?Printer URL not specified}" -P "${3:?PPD file path not specified}"
+}
+prn_delete() {
+  local NAME="${1:?Printer name not specified}"
+  sudo lpoptions -x "$NAME"
+  sudo lpadmin -x "$NAME"
 }
 
 # Printer options
@@ -18,13 +25,12 @@ prn_options() {
 }
 
 # Aliases
-alias prn_default='lpstat -p -d'
+alias prn_default='lpoptions -d -p'
 alias prn_print='lp'
 alias prn_enable='cupsenable'
 alias prn_disable='cupsdisable'
 alias prn_stat='lpstat'
 alias prn_id='lpq'
-alias prn_cancel='lprm'
 alias prn_move='lpmove'
-#alias prn_cancel='cancel'
-alias prn_uninstall='lpoptions -x'
+alias prn_cancel='cancel'
+#alias prn_cancel='lprm'
