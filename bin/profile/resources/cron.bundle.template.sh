ec00f878ac0e3a27b46ede25ae7cb45a9b9fa4f2
Oliv5 <oliv5@caramail.com>
2015-05-16 11:05:13 +0200

    [cron] update bundle backup template

--- bin/profile/resources/cron.bundle.template.sh
+++ bin/profile/resources/cron.bundle.template.sh
@@ -3,9 +3,9 @@ SRCDIR="${1:-/etc}"
 DSTDIR="${2:-/var/backups/system}"
 GPGKEY="${3}"
 BUNDLE="$DSTDIR/$(basename "$SRCDIR").bundle.$(uname -n).$(date +%Y%m%d-%H%M%S).git"
-command git -C "$SRCDIR" bundle create "$BUNDLE" --all --tags --remotes
+command -p git -C "$SRCDIR" bundle create "$BUNDLE" --all --tags --remotes
 if [ ! -z "$GPGKEY" ]; then
-    gpg -v --batch --no-default-recipient --recipient "$GPGKEY" --trust-model always --encrypt "$BUNDLE" && (
-	wipe -q -f "$BUNDLE" || rm "$BUNDLE"
+    command -p gpg -v --batch --no-default-recipient --recipient "$GPGKEY" --trust-model always --encrypt "$BUNDLE" && (
+	command -p wipe -q -f "$BUNDLE" || command -p rm "$BUNDLE"
     )
 fi
