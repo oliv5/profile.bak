f782729a723b17acdf2fdd128f2416fe67b1e166
Oliv5 <oliv5@caramail.com>
2015-05-26 19:23:29 +0200

    [path] make path_append/prepend/cleanup more generic and apply them to LD_LIBRARY_PATH

--- .rc.d/01_path.sh
+++ .rc.d/01_path.sh
@@ -1,36 +1,47 @@
 #!/bin/sh
 
 # Prepend to path
-path_prepend() {
+_path_prepend() {
+  local VAR="${1:-PATH}"
+  shift
   local DIR
   for DIR; do
-    #if ! [[ "$PATH" =~ "${DIR}" ]] && [[ -d "$DIR" ]]; then
-    if [ -d "$DIR" ] && ! (echo "$PATH" | grep "${DIR}" >/dev/null); then
-      export PATH="${DIR}${PATH:+:$PATH}"
+    if [ -d "$DIR" ] && ! (eval echo "\$$VAR" | grep "${DIR}" >/dev/null); then
+      eval export $VAR="${DIR}\${$VAR:+:\$$VAR}"
     fi
   done
 }
 
 # Append to path
-path_append() {
+_path_append() {
+  local VAR="${1:-PATH}"
+  shift
   local DIR
   for DIR; do
-    #if ! [[ "$PATH" =~ "${DIR}" ]] && [[ -d "$DIR" ]]; then
-    if [ -d "$DIR" ] && ! (echo "$PATH" | grep "${DIR}" >/dev/null); then
-      export PATH="${PATH:+$PATH:}${DIR}"
+    if [ -d "$DIR" ] && ! (eval echo "\$$VAR" | grep "${DIR}" >/dev/null); then
+      eval export $VAR="\${$VAR:+\$$VAR:}${DIR}"
     fi
   done
 }
 
 # Cleanup path
-path_cleanup() {
+_path_cleanup() {
+  local VAR="${1:-PATH}"
+  shift
   #PATH="${PATH//\~/${HOME}}"; PATH=${PATH//.:/}
   #PATH="$(echo "$PATH" | sed -r 's|~|'"${HOME}"'|g; s|\.\:||g' | awk -v RS=':' -v ORS=":" '!a[$1]++')"
-  PATH="$(echo "$PATH" | awk 'NF && !x[$0]++' RS='[:|\n]' ORS=':' | sed -r 's|~|'"${HOME}"'|g; s|\:\.||g; s|(^:\|:$)||')"
-  export PATH
+  eval export $VAR="$(echo "\$$VAR" | awk 'NF && !x[$0]++' RS='[:|\n]' ORS=':' | sed -r 's|~|'"${HOME}"'|g; s|\:\.||g; s|(^:\|:$)||')"
 }
 
-# Get absolute path
-path_abs() {
-  readlink -f -- "$@"
-}
+# Add to PATH
+alias path_prepend='_path_prepend PATH'
+alias path_append='_path_append PATH'
+alias path_cleanup='_path_cleanup PATH'
+alias path_abs='readlink -f --'
+
+# Add to LD_LIBRARY_PATH
+# Warning: should not use it
+# see ftp://linuxmafia.com/faq/Admin/ld-lib-path.html
+alias ldlibpath_prepend='_path_prepend LD_LIBRARY_PATH'
+alias ldlibpath_append='_path_append LD_LIBRARY_PATH'
+alias ldlibpath_cleanup='_path_cleanup LD_LIBRARY_PATH'
