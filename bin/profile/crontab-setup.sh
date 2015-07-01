a43a384fc42b5898ef5445a8f852ea1279580d45
Olivier Lanneluc <olanneluc@sequans.com>
2015-07-01 18:44:14 +0200

    [cron] add anacron support

--- bin/profile/crontab-setup.sh
+++ bin/profile/crontab-setup.sh
@@ -3,7 +3,7 @@ DIR="${1:-$HOME/bin/cron}"
 BACKUPDIR="${2:-/var/backups/$USER}"
 
 # Ask for backup script binary directory
-echo "Select backup scripts directory (default: '$DIR'): "
+echo "Select scripts directory (default: '$DIR'): "
 if ! { DIR=$(ask_file "" -n "$DIR"); }; then
 	echo "Error: unknown directory '$DIR'..."
 	return 1
@@ -11,48 +11,52 @@ fi
 mkdir -p "$DIR"
 echo
 
-# Create scripts
-BACKUP="$DIR/backup"
-if ask_question "Create backup scripts? (y/n) " y Y >/dev/null; then
-	touch "$BACKUP.cron"; 						chmod +x "$BACKUP.cron"
-	echo "#!/bin/sh" > "$BACKUP.minutly.sh";	chmod +x "$BACKUP.minutly.sh"
-	echo "#!/bin/sh" > "$BACKUP.hourly.sh";		chmod +x "$BACKUP.hourly.sh"
-	echo "#!/bin/sh" > "$BACKUP.daily.sh";		chmod +x "$BACKUP.daily.sh"
-	echo "#!/bin/sh" > "$BACKUP.weekly.sh";		chmod +x "$BACKUP.weekly.sh"
-	echo "#!/bin/sh" > "$BACKUP.monthly.sh";	chmod +x "$BACKUP.monthly.sh"
-	echo "#!/bin/sh" > "$BACKUP.yearly.sh";		chmod +x "$BACKUP.yearly.sh"
-	# Create rules
-	grep "$BACKUP.minutly.sh" "$BACKUP.cron" 	>/dev/null || echo "## *  * * * * $USER sh -c \"$BACKUP.minutly.sh\""	>> "$BACKUP.cron"
-	grep "$BACKUP.hourly.sh" "$BACKUP.cron" 	>/dev/null || echo "0  * * * * $USER sh -c \"$BACKUP.hourly.sh\""	>> "$BACKUP.cron"
-	grep "$BACKUP.daily.sh" "$BACKUP.cron" 		>/dev/null || echo "10 0 * * * $USER sh -c \"$BACKUP.daily.sh\""	>> "$BACKUP.cron"
-	grep "$BACKUP.weekly.sh" "$BACKUP.cron" 	>/dev/null || echo "20 0 * * 1 $USER sh -c \"$BACKUP.weekly.sh\""	>> "$BACKUP.cron"
-	grep "$BACKUP.monthly.sh" "$BACKUP.cron" 	>/dev/null || echo "30 0 1 * * $USER sh -c \"$BACKUP.monthly.sh\""	>> "$BACKUP.cron"
-	grep "$BACKUP.yearly.sh" "$BACKUP.cron" 	>/dev/null || echo "40 0 1 1 * $USER sh -c \"$BACKUP.yearly.sh\""	>> "$BACKUP.cron"
-fi
-echo
-
-# Make system or local cron links
-if ask_question "Create cron links in system /etc/cron* directory? (y/n) " y Y >/dev/null; then
-	sudo sudo ln -s "$BACKUP.cron" "/etc/cron.d/backup.$USER"
-elif { echo; ask_question "Add in user crontab instead? (y/n) " y Y >/dev/null; }; then
-	CRONTAB="$(mktemp)"
-	crontab -l > "$CRONTAB"
-	grep "$BACKUP.cron" "$CRONTAB" >/dev/null && echo "Rule already there, skip it." || {
-		printf '\n%s\n' "## from $BACKUP.cron (do not remove this line)" >> "$CRONTAB"
-		cat "$BACKUP.cron" | awk '!/^.*#/ && /'$USER'/ {$6="";print $0}' >> "$CRONTAB"
-		printf '\n%s\n' "## from $BACKUP.cron (do not remove this line)" >> "$CRONTAB"
-		crontab "$CRONTAB"
-	}
-	crontab -l
-	rm "$CRONTAB"
-fi
-echo
-
 # Make backup directory
 if ask_question "Create backup directory in '$BACKUPDIR'? (y/n) " y Y >/dev/null; then
 	sudo mkdir -p "$BACKUPDIR"
 	sudo chown $USER:$USER -R "$BACKUPDIR"
 	sudo chmod 700 "$BACKUPDIR"
+fi
+echo
+
+# Create crontabs
+BACKUP="$DIR/backup"
+if ask_question "Create crontabs? (y/n) " y Y >/dev/null; then
+	# Write crontab rules
+	echo > "$BACKUP.crontab" <<EOF
+# Edit this file to introduce tasks to be run by cron.
+# 
+# Each task to run has to be defined through a single line
+# indicating with different fields when the task will be run
+# and what command to run for the task
+# 
+
+# m h  dom mon dow   command
+## *  * * * * $USER sh -c "$BACKUP.minutly.sh"
+0  * * * * $USER sh -c "$BACKUP.hourly.sh"
+10 0 * * * $USER sh -c "$BACKUP.daily.sh"
+20 0 * * 1 $USER sh -c "$BACKUP.weekly.sh"
+30 0 1 * * $USER sh -c "$BACKUP.monthly.sh"
+40 0 1 1 * $USER sh -c "$BACKUP.yearly.sh"
+
+EOF
+
+	# Write anacrontab rules
+	echo > "$BACKUP.anacrontab" <<EOF
+# /etc/anacrontab: configuration file for anacron
+# See anacron(8) and anacrontab(5) for details.
+#
+
+SHELL=/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+# period  delay  job-identifier  command
+1        10 backup.daily   "$BACKUP.daily.sh"
+7        20 backup.weekly  "$BACKUP.weekly.sh"
+@monthly 30 backup.monthly "$BACKUP.monthly.sh"
+365      40 backup.yearly  "$BACKUP.yearly.sh"
+
+EOF
 fi
 echo
 
@@ -72,5 +76,32 @@ for SCRIPT in "$BACKUP.minutly.sh" "$BACKUP.hourly.sh" "$BACKUP.daily.sh" "$BACK
 			cp "$TEMPLATE" "$SCRIPT"
 		fi
 	fi
+	if [ -f "$SCRIPT" ]; then
+		chmod +x "$SCRIPT"
+	fi
 	echo
 done
+
+# Make system or local cron links
+if ask_question "Create cron links in system /etc/cron* directory? (y/n) " y Y >/dev/null; then
+	sudo ln -s "$BACKUP.crontab" "/etc/cron.d/backup.$USER"
+elif { echo; ask_question "Add in user crontab instead? (y/n) " y Y >/dev/null; }; then
+	CRONTAB="$(mktemp)"
+	crontab -l > "$CRONTAB"
+	grep "$BACKUP.crontab" "$CRONTAB" >/dev/null && echo "Rule already there, skip it." || {
+		printf '\n%s\n' "## from $BACKUP.crontab (do not remove this line)" >> "$CRONTAB"
+		cat "$BACKUP.crontab" | awk '!/^.*#/ && /'$USER'/ {$6="";print $0}' >> "$CRONTAB"
+		printf '\n%s\n' "## from $BACKUP.crontab (do not remove this line)" >> "$CRONTAB"
+		crontab "$CRONTAB"
+	}
+	crontab -l
+	rm "$CRONTAB"
+elif { echo; ask_question "Add in system anacrontab instead? (y/n) " y Y >/dev/null; }; then
+	grep "$BACKUP.anacrontab" /etc/anacrontab >/dev/null && echo "Rule already there, skip it." || {
+		sudo cat "$BACKUP.anacrontab" >> /etc/anacrontab
+	}
+elif { echo; ask_question "Create user anacrontab instead? (y/n) " y Y >/dev/null; }; then
+	mkdir -p "${HOME}/.anacron"
+	ln -s "$BACKUP.anacrontab" "${HOME}/.anacron/anacrontab"
+fi
+echo
