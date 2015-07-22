ee0912039018699ca64034fcf0275cdcaeab41e2
Olivier Lanneluc <olanneluc@sequans.com>
2015-07-22 17:45:16 +0200

    [cron] update backup template script

--- bin/profile/resources/cron.backup.template.sh
+++ bin/profile/resources/cron.backup.template.sh
@@ -9,17 +9,38 @@ EXCLUSIONS="$EXCLUSIONS --exclude=tmp --exclude=temp --exclude=cache"
 EXCLUSIONS="$EXCLUSIONS --exclude=.tmp --exclude=.temp --exclude=.cache"
 
 # Set archive variable
-ARCHIVE="${0##*/}"
-ARCHIVE="${ARCHIVE%.*}.$(date +%Y%m%d_%H%M%S)"
+PREAMBLE="${0##*/}"
+PREAMBLE="${PREAMBLE%.*}"
+ARCHIVE="${PREAMBLE}.$(date +%Y%m%d_%H%M%S)"
 
-# Make directory
-mkdir -p "$DIR"
-chown $USER:$USER -R "$DIR"
+# Main script redirection
+{
+    # Startup
+    date
 
-# Delete file too old (mtime=nb days)
-find "$DIR" -maxdepth 1 -name 'backup.tar*' -mtime +27 -type f -delete
+    # Make directory
+    echo "[$0] create directories..."
+    mkdir -p "$DIR"
+    chown $USER:$USER -R "$DIR"
+    echo
 
-# Do the backup
-tar -cvpjf "$DIR/${ARCHIVE}.tar.bz" --one-file-system $EXCLUSIONS $INCLUSIONS | tee "$DIR/${ARCHIVE}.log"
-du -h -d 1 "$DIR/*" . | tee -a "$DIR/${ARCHIVE}.log"
+    # Delete file too old (mtime=nb days)
+    echo "[$0] delete old files..."
+    find "$DIR" -maxdepth 1 -name "${PREAMBLE}"'*' -mtime +27 -type f -print -delete
+    echo
+
+    # Do the backup
+    echo "[$0] backup files..."
+    tar -cvpjf "$DIR/${ARCHIVE}.tar.bz" --one-file-system $EXCLUSIONS $INCLUSIONS
+    echo
+
+    # Display snapshot
+    echo "[$0] Show the results..."
+    du -h -d 1 "$DIR" -a
+    echo
+    
+    # End
+    date
+
+} 2>&1 | tee -a "$DIR/${ARCHIVE}.log"
 ##### Cron backup (end)
