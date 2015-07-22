a3c72a93bb33558ff2ac83c4823b766e854fcd55
Olivier Lanneluc <olanneluc@sequans.com>
2015-07-22 18:35:05 +0200

    [cron] remove etckeeper bundle script, replaced by cron.bundle script + add support for shred in cron.bundle script

--- bin/profile/etckeeper-bundle.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-DIR="/home/olivier/repo/sync/git/bundle"
-sudo /usr/bin/git -C /etc bundle create "$DIR/etc.bundle.$(uname -n).$(date +%Y%m%d-%H%M%S).git" --all --tags --remotes
--- bin/profile/resources/cron.bundle.template.sh
+++ bin/profile/resources/cron.bundle.template.sh
@@ -6,6 +6,6 @@ BUNDLE="$DSTDIR/$(basename "$SRCDIR").bundle.$(uname -n).$(date +%Y%m%d-%H%M%S).
 command -p git -C "$SRCDIR" bundle create "$BUNDLE" --all --tags --remotes
 if [ ! -z "$GPGKEY" ]; then
     command -p gpg -v --batch --no-default-recipient --recipient "$GPGKEY" --trust-model always --encrypt "$BUNDLE" && (
-	command -p wipe -q -f "$BUNDLE" || command -p rm "$BUNDLE"
+	command -p shred -v "$BUNDLE" || command -p wipe -q -f "$BUNDLE" || command -p rm "$BUNDLE"
     )
 fi
