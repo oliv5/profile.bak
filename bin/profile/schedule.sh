e3bfcd613ebc13bc4a940f164b5326cf9f44bfd7
Olivier Lanneluc <olanneluc@sequans.com>
2015-07-28 17:09:01 +0200

    [schedule] can now be rescheduled regularly using watch

--- bin/profile/schedule.sh
+++ bin/profile/schedule.sh
@@ -7,14 +7,17 @@ TIMEOUT=""
 TIMEOUT_OPT=""
 RETRY=""
 RETRY_OPT=0
+WATCH=""
 
 # Get args
-while getopts "t:mbl:k:s:r:p:h" FLAG
+while getopts "t:mbw:l:k:s:r:p:h" FLAG
 do
   case "$FLAG" in
     t) AT="${OPTARG}";;
     m) AT_OPT="${AT_OPT:+$AT_OPT }-m";;
     b) BATCH="batch";;
+    
+    w) WATCH="-n ${OPTARG}";;
     
     l) TIMEOUT="${OPTARG}";;
     k) TIMEOUT_OPT="${TIMEOUT_OPT:+$TIMEOUT_OPT }-k ${OPTARG}";;
@@ -23,15 +26,16 @@ do
     r) RETRY="${OPTARG}";;
     p) RETRY_OPT="${OPTARG}";;
     
-    h) echo >&2 "Usage: `basename $0` [-t time] [-m] [-b] [-l length] [-k time] -[s signal] [-r trials] [-p pause] -- <command line...>"
+    h|*) echo >&2 "Usage: `basename $0` [-t time] [-m] [-b] [-w seconds] [-l seconds] [-k seconds] -[s signal] [-r trials] [-p pause] -- <command line...>"
        echo >&2 "-t   at: time of execution (man at)"
        echo >&2 "-m   at: send email upon completion"
        echo >&2 "-b   batch: execute when system load < 1.5%"
-       echo >&2 "-l   timeout: timeout length (s/m/h/d)"
-       echo >&2 "-k   timeout: kill after N sec upon timeout"
+       echo >&2 "-w   watch: execute every N seconds"
+       echo >&2 "-l   timeout: timeout length in seconds"
+       echo >&2 "-k   timeout: kill after N seconds upon timeout"
        echo >&2 "-s   timeout: signal name to send upon timeout"
-       echo >&2 "-r   retry: retry N times upon failure"
-       echo >&2 "-p   retry: pause S sec between each trial"
+       echo >&2 "-r   retry: retry N times upon failure (-1 inf)"
+       echo >&2 "-p   retry: pause S seconds between each trial"
        echo >&2 "...  command line to execute"
        exit 1
        ;;
@@ -42,10 +46,19 @@ CMDLINE="$@"
 
 # Build control command lines
 AT="${BATCH:-${AT:+at $AT $AT_OPT}}"
-TIMEOUT="${TIMEOUT:+timeout $TIMEOUT_OPT $TIMEOUT }"
-RETRY="${RETRY:+retry.sh $RETRY $RETRY_OPT }"
+WATCH="${WATCH:+watch $WATCH}"
+TIMEOUT="${TIMEOUT:+timeout $TIMEOUT_OPT $TIMEOUT}"
+RETRY="${RETRY:+retry.sh $RETRY $RETRY_OPT}"
 
-# Build the final command line
-CMDLINE="${CMDLINE:+${AT:+${AT} <<EOF\n}${TIMEOUT}${RETRY}${CMDLINE}${AT:+\nEOF\n}}"
-${DGB} eval "$(printf "$CMDLINE")"
+# Build the final command-line and execute
+CMDLINE="${CMDLINE:-false}"
+[ ! -z "$RETRY" ] && CMDLINE="${RETRY} ${CMDLINE}"
+[ ! -z "$TIMEOUT" ] && CMDLINE="${TIMEOUT} sh -c '${CMDLINE}'"
+[ ! -z "$WATCH" ] && CMDLINE="${WATCH} -- ${CMDLINE}"
+if [ ! -z "$AT" ]; then
+  CMDLINE="$(printf "${AT} <<EOF\n${CMDLINE}\nEOF\n")"
+  ${DGB} eval "${CMDLINE}"
+else
+  ${DGB} ${CMDLINE}
+fi
 exit 0
