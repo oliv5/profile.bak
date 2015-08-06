096906e63217605cc26270189d26633cfbed9619
Olivier Lanneluc <olanneluc@sequans.com>
2015-08-06 19:11:48 +0200

    [schedule] now detect when times format are wrong + add debug&verbose modes

--- bin/profile/schedule.sh
+++ bin/profile/schedule.sh
@@ -8,28 +8,38 @@ TIMEOUT_OPT=""
 RETRY=""
 RETRY_OPT=0
 WATCH=""
+VERBOSE=""
 
-# Convert the input into seconds
-toSec(){
-  echo "$1" | awk -F'[:.]' '{ for(i=0;i<2;i++){if(NF<=2){$0=":"$0}}; print ($1 * 3600) + ($2 * 60) + $3 }'
+# Convert OPTARG into seconds
+arg2Sec(){
+  OPTARG="$(echo "$OPTARG" | awk -F'[:.]' '{ for(i=0;i<2;i++){if(NF<=2){$0=":"$0}}; print ($1 * 3600) + ($2 * 60) + $3 }')"
+  if [ $OPTARG -eq 0 ]; then
+    echo "Invalid '-$OPTFLAG' parameter. Abort..."
+    exit 2
+  fi
 }
 
 # Get args
-while getopts "t:mbw:l:k:s:r:p:h" FLAG
+OPTIND=0
+unset OPTFLAG OPTARG OPTERR
+while getopts "t:mbw:l:k:s:r:p:hdv" OPTFLAG
 do
-  case "$FLAG" in
+  case "$OPTFLAG" in
     t) AT="${OPTARG}";;
     m) AT_OPT="${AT_OPT:+$AT_OPT }-m";;
     b) BATCH="batch";;
     
-    w) WATCH="-n $(toSec ${OPTARG})";;
+    w) arg2Sec; WATCH="-n ${OPTARG}";;
     
-    l) TIMEOUT="$(toSec ${OPTARG})";;
-    k) TIMEOUT_OPT="${TIMEOUT_OPT:+$TIMEOUT_OPT }-k $(toSec ${OPTARG})";;
+    l) arg2Sec; TIMEOUT="${OPTARG}";;
+    k) arg2Sec; TIMEOUT_OPT="${TIMEOUT_OPT:+$TIMEOUT_OPT }-k ${OPTARG}";;
     s) TIMEOUT_OPT="${TIMEOUT_OPT:+$TIMEOUT_OPT }-s ${OPTARG}";;
     
     r) RETRY="${OPTARG}";;
-    p) RETRY_OPT="$(toSec ${OPTARG})";;
+    p) arg2Sec; RETRY_OPT="${OPTARG}";;
+    
+    d) DBG="echo";;
+    v) VERBOSE="x";;
     
     h|*) echo >&2 "Usage: `basename $0` [-t time] [-m] [-b] [-w h:m:s] [-l h:m:s] [-k h:m:s] -[s signal] [-r trials] [-p h:m:s] -- <command line...>"
        echo >&2 "-t   at: time of execution (man at)"
@@ -41,13 +51,17 @@ do
        echo >&2 "-s   timeout: kill signal name sent upon timeout"
        echo >&2 "-r   retry: nb of retries upon failure (-1 inf)"
        echo >&2 "-p   retry: pause/delay between each trial"
+       echo >&2 "-d   enable debug mode"
+       echo >&2 "-v   enable verbose mode"
        echo >&2 "...  command line to execute"
        exit 1
        ;;
   esac
 done
 shift $(($OPTIND-1))
+unset OPTFLAG OPTARG OPTERR
 CMDLINE="$@"
+OPTIND=0
 
 # Build control command lines
 AT="${BATCH:-${AT:+at $AT $AT_OPT}}"
@@ -62,8 +76,8 @@ CMDLINE="${CMDLINE:-false}"
 [ ! -z "$WATCH" ] && CMDLINE="${WATCH} -- ${CMDLINE}"
 if [ ! -z "$AT" ]; then
   CMDLINE="$(printf "${AT} <<EOF\n${CMDLINE}\nEOF\n")"
-  ${DGB} eval "${CMDLINE}"
+  (set -${VERBOSE}; $DBG eval "${CMDLINE}")
 else
-  ${DGB} ${CMDLINE}
+  (set -${VERBOSE}; ${DGB} ${CMDLINE})
 fi
 exit 0
