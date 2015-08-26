5f95660974b287e62b72e1551d6f43e05808bb34
Olivier Lanneluc <olanneluc@sequans.com>
2015-08-26 19:30:33 +0200

    [annex] move files into .config/profile + remove unused scripts + move mr/vcsh binaries into resources + update profile_setup.sh

--- bin/profile/profile-setup.sh
+++ bin/profile/profile-setup.sh
@@ -4,7 +4,7 @@
 command -v git 2>&1 >/dev/null || (echo "Git missing, cannot go on..." && exit 1)
 
 # Goto home directory
-cd $HOME
+cd "$HOME"
 mkdir bin 2>/dev/null
 
 # Download and install vcsh if not already there
@@ -13,5 +13,12 @@ if ! command -v vcsh >/dev/null 2>&1; then
 	export PATH="$PATH:$HOME/bin/vcsh"
 fi
 
+# Download and install mr if not already there
+if ! command -v mr >/dev/null 2>&1; then
+	git clone https://github.com/joeyh/myrepos.git "$HOME/bin/mr"
+	export PATH="$PATH:$HOME/bin/mr"
+fi
+
 # Get profile repository
-vcsh clone ssh://olivier@oliv5kta.dtdns.net:443/home/olivier/git/profile.git
+vcsh clone ssh://olivier@oliv5kta.dtdns.net:443/home/olivier/git/profile.git ||
+vcsh clone https://github.com/oliv5/profile.git
