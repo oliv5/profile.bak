bfbc028fa9cf5c755946dc10fda201c1a9747615
Olivier Lanneluc <olanneluc@sequans.com>
2015-08-28 16:16:02 +0200

    [bin] update profile-setup.sh

--- bin/profile/profile-setup.sh
+++ bin/profile/profile-setup.sh
@@ -1,33 +1,57 @@
 #!/bin/sh
+set -e
 
+###################
 # Check prerequisites
-command -v git 2>&1 >/dev/null || (echo "Git missing, cannot go on..." && exit 1)
+if ! command -v git 2>&1 >/dev/null; then 
+	echo "Git missing, cannot go on..."
+	exit 1
+fi
 
-# Goto home directory
-cd "$HOME"
-mkdir -p bin/externals 2>/dev/null
+# Ask for private directory
+read -p "Enter private data directory in \$HOME: " PRIVATE
+export PRIVATE="$HOME/$PRIVATE"
 
+###################
 # Download and install vcsh if not already there
 if ! command -v vcsh >/dev/null 2>&1; then
 	git clone https://github.com/RichiH/vcsh.git "$HOME/bin/vcsh"
 	export PATH="$PATH:$HOME/bin/vcsh"
 fi
 
+# Clone profile repository
+cd "$HOME"
+vcsh clone https://github.com/oliv5/profile.git
+
+###################
 # Download and install mr if not already there
 if ! command -v mr >/dev/null 2>&1; then
 	git clone https://github.com/joeyh/myrepos.git "$HOME/bin/mr"
 	export PATH="$PATH:$HOME/bin/mr"
 fi
 
+# Checkout mr repositories
+cd "$HOME"
+mr checkout
+
+###################
 # Download and install google repo if not already there
 if ! command -v repo >/dev/null 2>&1; then
-	curl https://storage.googleapis.com/git-repo-downloads/repo > "$HOME/bin/externals/repo" && chmod a+x "$HOME/bin/externals/repo"
-	git clone https://gerrit.googlesource.com/git-repo "$HOME/bin/git-repo" && chmod a+x "$HOME/bin/git-repo/repo"
+	mkdir -p bin/externals
+	curl https://storage.googleapis.com/git-repo-downloads/repo > "$HOME/bin/externals/repo" && 
+		chmod a+x "$HOME/bin/externals/repo"
+	git clone https://gerrit.googlesource.com/git-repo "$HOME/bin/git-repo" && 
+		chmod a+x "$HOME/bin/git-repo/repo"
 	export PATH="$PATH:$HOME/bin/externals:$HOME/bin/git-repo"
 fi
 
-# Clone profile repository
-vcsh clone https://github.com/oliv5/profile.git
-
-# Clone external repositories
+# Init bin repositories
+mkdir -p "$HOME/bin"
+cd "$HOME/bin"
 repo init -u https://github.com/oliv5/manifests.git -m bin.xml
+
+###################
+# Init dev repositories
+mkdir -p "$PRIVATE/dev"
+cd "$PRIVATE/dev"
+repo init -u https://github.com/oliv5/manifests.git -m dev.xml
