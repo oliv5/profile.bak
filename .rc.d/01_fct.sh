c5b2e954eab9e714b8cecbc38da4ce7e417ec29d
Olivier Lanneluc <olanneluc@sequans.com>
2015-08-28 20:59:11 +0200

    [rc] add fct_exists and fct_collision

--- .rc.d/01_fct.sh
+++ .rc.d/01_fct.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
 ################################
+# Check fct exists
+fct_exists() {
+  true ${1:?No fct specified}
+  set | grep -G "^$1\s*()" >/dev/null 2>&1
+  # The following code returns false when
+  # function is overriden by an existing alias
+  #[ "$(type -t $1)" = "function" ]
+}
+
 # Get fct definition
 fct_def() {
   true ${1:?No fct specified}
-  if [ "$(type -t ${1:?No fct specified})" = "function" ]; then
+  if fct_exists "$1"; then
     type $1 | tail -n +2
   fi
 }
@@ -12,7 +21,7 @@ fct_def() {
 # Get fct content
 fct_content() {
   true ${1:?No fct specified}
-  if [ "$(type -t $1)" = "function" ]; then
+  if fct_exists "$1"; then
     type $1 | head -n -1 | tail -n +4
   fi
 }
@@ -27,6 +36,15 @@ fct_append() {
 fct_prepend() {
   local FCT=${1:?No fct specified}; shift
   eval "${FCT}() { $@; $(fct_content $FCT); }"
+}
+
+# Check alias/fct collision
+fct_collision() {
+  for ALIAS in $(alias | awk -F '[= ]' '{print $2}'); do
+    [ "${1:-$ALIAS}" = "$ALIAS" ] &&
+    fct_exists "$ALIAS" && 
+    echo "$ALIAS"
+  done
 }
 
 ################################
