764a3f9618de461066864e27062a74cc443688ea
Oliv5 <oliv5@caramail.com>
2015-09-13 15:30:57 +0200

    [pbin] fix gps_scripter.sh to run properly on android

--- pbin/gps_scripter.sh
+++ pbin/gps_scripter.sh
@@ -2,15 +2,10 @@
 # Useful actions to execute:
 # adb shell svc wifi enable
 # adb shell svc wifi disable
-
-main() {
+(
     local LOCATION_FILE="${1:-.locations}"
     # Check prerequisites
-    if ! command -v bc >/dev/null 2>&1; then
-        echo >&2 "[error] Cannot find bc. Abort..."
-        exit 1
-    fi
-    if ! command -v gps.sh >/dev/null 2>&1; then
+    if [ -z "$ANDROID_ROOT" ] && ! command -v gps.sh >/dev/null 2>&1; then
         echo >&2 "[error] Cannot find gps.sh. Abort..."
         exit 1
     fi
@@ -23,12 +18,9 @@ main() {
         while IFS=$' ' read LOC OP DIST ACTION; do
             if [ -n "$ACTION" ]; then
                 DISTANCE=$(${ANDROID_ROOT:+.} gps.sh dist "$HERE" "$LOC" 0)
-                if expr $(($DISTANCE)) $OP $(($DIST)) >/dev/null; then
+                if expr ${DISTANCE:-0} $OP ${DIST:-0} >/dev/null; then
                     eval "$ACTION"
                 fi
             fi
         done
-}
-
-# Just do it
-main "$@"
+)
