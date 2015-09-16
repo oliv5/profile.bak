7f76e9fc891a467266e6e1921dd088963edec107
Olivier Lanneluc <olanneluc@sequans.com>
2015-09-16 10:37:48 +0200

    [abin] move all scripts into android home repo

--- abin/annex_shell.sh
+++
@@ -1,4 +0,0 @@
-#!/system/bin/sh
-ANNEX_BIN="/data/data/ga.androidterm/runshell"
-USER="$(ls -l "$ANNEX_BIN" | awk 'NR>1 {print $3}')"
-su "$USER" -c "$ANNEX_BIN"
--- abin/bug_checkinservice.sh
+++
@@ -1,2 +0,0 @@
-#!/system/bin/sh
-pm disable com.google.android.gms/com.google.android.gms.checkin.CheckinService
