2fe7d10df61e995294a91e50b473c94a5e6e73d0
Olivier Lanneluc <olanneluc@sequans.com>
2015-10-14 12:24:50 +0200

    [desktop] fix desktop files & instll script

--- .config/profile/applications/install.sh
+++ .config/profile/applications/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 SRCPATH="${1:-$PWD}"
 for FILE in "${SRCPATH}"/*.desktop; do
-    ln -sv "${FILE}" "${HOME}/.local/share/applications/$(basename "${FILE}")"
+    ln -fsv "${FILE}" "${HOME}/.local/share/applications/$(basename "${FILE}")"
 done
