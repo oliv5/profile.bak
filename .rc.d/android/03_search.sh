1ace981fa01d155f0810a214caedeea201843f44
Olivier nexus4 <oliv5@caramail.com>
2015-11-23 20:28:45 +0100

    [rc] add _ffind for android.

---
+++ .rc.d/android/03_search.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Find files implementations
+_ffind3() {
+  local FCASE="${FCASE:--}name"
+  local FILES="${1##*/}"
+  local DIR="${1%"$FILES"}"
+  shift 2>/dev/null
+  local REGEX='s/;!/" -o -not '${FCASE}' "/g ; s/&!/" -a -not '${FCASE}' "/g ; s/;/" -o '${FCASE}' "/g ; s/&/" -a '${FCASE}' /g'
+  ( set -f; FILES="\"$(echo $FILES | sed -e "$REGEX")\""
+    eval find "${DIR:-.}" ${FTYPE:+-type $FTYPE} ${FILES:+$FCASE "$FILES"} ${FARGS} "$@")
+}
+alias _ffind='_ffind3'
+
+
