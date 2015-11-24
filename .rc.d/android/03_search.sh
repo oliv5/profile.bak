1b457724deb99d7ebffd0c860e7ecbe1e693789f
Olivier Lanneluc <olanneluc@sequans.com>
2015-11-24 17:43:22 +0100

    [rc] android _ffind3 shift does not produce errors anymore so set -e does not complain.

--- .rc.d/android/03_search.sh
+++ .rc.d/android/03_search.sh
@@ -5,7 +5,7 @@ _ffind3() {
   local FCASE="${FCASE:--}name"
   local FILES="${1##*/}"
   local DIR="${1%"$FILES"}"
-  shift 2>/dev/null
+  ${1:-true} || shift
   local REGEX='s/;!/" -o -not '${FCASE}' "/g ; s/&!/" -a -not '${FCASE}' "/g ; s/;/" -o '${FCASE}' "/g ; s/&/" -a '${FCASE}' /g'
   ( set -f; FILES="\"$(echo $FILES | sed -e "$REGEX")\""
     eval find "${DIR:-.}" ${FTYPE:+-type $FTYPE} ${FILES:+$FCASE "$FILES"} ${FARGS} "$@")
