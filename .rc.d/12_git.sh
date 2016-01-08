e23aebd466b0179f310b39b00d117fbfafdcf8d8
Olivier Lanneluc <olanneluc@sequans.com>
2016-01-08 15:24:26 +0100

    [rc] add few aliases.

--- .rc.d/12_git.sh
+++ .rc.d/12_git.sh
@@ -42,7 +42,8 @@ alias gdiff='git diff'
 alias gmm='git mergetool -y'
 alias gmerge='gmm'
 # Branch aliases
-alias gba='git branch -a'
+alias gba='git branch -a'   # all branches
+alias gbl='git branch -l'   # local branches
 alias gbv='git branch -v'   # verbose list branch
 alias gbva='git branch -va' # verbose list branch
 alias gbd='git branch -d'   # branch delete
