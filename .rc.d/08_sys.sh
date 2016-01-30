72177201b4de9e471ef00054417ca1b7f4ba5da0
Oliv5 <oliv5@caramail.com>
2016-01-30 16:28:12 +0100

    [rc] add user_rename()

--- .rc.d/08_sys.sh
+++ .rc.d/08_sys.sh
@@ -71,3 +71,42 @@ cron_useradd() {
 cron_sysadd() {
   sudo sh -c 'echo "$@" >> "/etc/cron.d/$USER"'
 }
+
+################################
+# Rename current logged on user and its group
+user_rename_current_user() {
+  local OLDNAME="${1:?Old name not specified...}"
+  local NEWNAME="${2:?New name not specified...}"
+  local DESCR="${3:-$NEWNAME}"
+  local CURNAME="$(whoami)"
+  echo "List of commands to carry on:"
+  echo
+  cat <<EOF
+exit
+ssh $CURNAME@$HOSTNAME "sudo useradd tempuser; sudo passwd tempuser; sudo usermod -a -G sudo tempuser"
+ssh tempuser@$HOSTNAME "/bin/sh -c '$(type user_rename | tail -n +2); user_rename $OLDNAME $NEWNAME $DESCR'"
+ssh $CURNAME@$HOSTNAME "sudo userdel tempuser"
+EOF
+}
+
+# Rename another user and its group
+user_rename() {
+  local OLDNAME="${1:?Old name not specified...}"
+  local NEWNAME="${2:?New name not specified...}"
+  local DESCR="${3:-$NEWNAME}"
+  local CURNAME="$(whoami)"
+  if [ "$CURNAME" = "$OLDNAME" ]; then
+    echo "Cannot rename the current logged on user."
+    user_rename_current_user "$@"
+    return 1
+  else
+    sudo killall -u "$OLDNAME"
+    sudo id "$OLDNAME"
+    sudo usermod -l "$NEWNAME" "$OLDNAME"
+    sudo groupmod -n "$NEWNAME" "$OLDNAME"
+    sudo usermod -d /home/"$NEWNAME" -m "$NEWNAME"
+    sudo usermod -c "$DESCR" "$NEWNAME"
+    sudo id "$NEWNAME"
+    return 0
+  fi
+}
