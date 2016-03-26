5a1e106ac8935250e8fb80e9166af3b57610fcb7
Oliv5 <oliv5@caramail.com>
2016-03-26 18:19:32 +0100

    [upkeep] auto-commit

--- .rc.d/12_git.sh
+++ .rc.d/12_git.sh
@@ -458,7 +458,6 @@ git_upkeep() {
   git_exists || return 1
   vcsh_run git status
   if [ "$1" = "-y" ] || ask_question "Add and commit new files? (y/n): " y Y >/dev/null; then
-	set -vx
     vcsh_run git add -u :/
     vcsh_run git commit -m '[upkeep] auto-commit'
   fi
