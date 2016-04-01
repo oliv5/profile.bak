5224d71b19b0558b9fb49f50554e630f0919f964
Olivier Lanneluc <olanneluc@sequans.com>
2016-04-01 14:33:41 +0200

    git-annex automatic sync

--- .vim-spf13/install.sh
+++ .vim-spf13/install.sh
@@ -1,23 +1,57 @@
 #!/bin/sh
+date="$(date +%Y%m%d-%H%M%S)"
+backup_dir="~/.vimdata/spf13backup"
+setup_type="${1:-full}"
 
-set -vx
-# Backup current config
-DATE=`date +%Y%m%d`
-mkdir -p ~/.vimdata/vimbackup
-for FILE in ~/.vim ~/.vimrc*; do
-	[ -e "$FILE" ] || continue
-	if [ -L "$FILE" ]; then
-		rm -v "$FILE"
-	else
-		ls -l "$FILE"
-		mv -v "$FILE" "$HOME/.vimdata/vimbackup/$(basename "$FILE").$DATE.bak"
+############
+# Functions
+
+# Full setup: spf13 everywhere
+full_setup() {
+	ln -fsv ~/.spf13-vim-3/.vim* ~/
+	ln -fsv ~/.vim-spf13/.vimrc* ~/
+	mkdir -p ~/.vim/plugin
+	for plugin in ~/.vim-legacy/.vim/plugin/*; do
+		ln -fsv "$plugin" ~/.vim/plugin/
+	done
+	rm ~/.gvimrc 2>/dev/null
+}
+
+# Mixed setup: gvim=spf13, vim=vim-legacy
+mixed_setup() {
+	full_setup
+	ln -fsv ~/.spf13-vim-3/.vimrc ~/.gvimrc
+	rm ~/.vimrc 2>/dev/null
+	echo "source ~/.vim-legacy/.vimrc" > ~/.vimrc
+}
+
+# Raw setup: gvim=spf13, vim=raw
+raw_setup() {
+	full_setup
+	ln -fsv ~/.spf13-vim-3/.vimrc ~/.gvimrc
+	rm ~/.vimrc 2>/dev/null
+	echo "" > ~/.vimrc
+}
+
+############
+# Main
+
+# Make backup dir
+mkdir -p "$backup_dir"
+
+# Backup & remove current config
+echo "Backup and remove current setup."
+for file in ~/.vim ~/.vimrc* ~/.vimrc*; do
+	if [ -f "$file" ]; then
+		cp -v "$file" "$backup_dir/$(basename "$file").$date.bak"
 	fi
+	rm -v "$file" 2>/dev/null
 done
 
-# Setup all things
-ln -fsv ~/.spf13-vim-3/.vim* ~/
-ln -fsv ~/.vim-spf13/.vimrc.* ~/
-mkdir -p ~/.vim/plugin
-for PLUGIN in ~/.vim-legacy/.vim/plugin/*; do
-	ln -fsv "$PLUGIN" ~/.vim/plugin/
-done
+# Proceed with installation
+case "$setup_type" in
+	full) echo "Proceed with full SPF13 setup."; full_setup;;
+	mixed) echo "Proceed with mixed SPF13/vim-legacy setup."; mixed_setup;;
+	raw) echo "Proceed with SPF13/raw vim setup."; raw_setup;;
+	*) echo "No setup.";;
+esac
