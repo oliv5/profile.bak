13ab4a071a5402b61f5b2189596f832d3f478450
Oliv5 <oliv5@caramail.com>
2016-06-04 00:16:53 +0200

    [rc] add git aliases.

--- .rc.d/12_git.sh
+++ .rc.d/12_git.sh
@@ -7,9 +7,7 @@ export GIT_PAGER="${PAGER:-less}"
 ########################################
 # Status aliases
 alias gt='git status -uno'
-alias gtu='git status -u'
 alias gst='git_st'
-alias gstx='git_stx'
 alias gstm='git status --porcelain -b | awk "NR==1 || /^(M.|.M)/"'    # modified
 alias gsta='git status --porcelain -b | awk "NR==1 || /^A[ MD]/"'     # added
 alias gstd='git status --porcelain -b | awk "NR==1 || /^D[ M]|^ D/"'  # deleted
@@ -20,7 +18,18 @@ alias gstu='git status --porcelain -b | awk "NR==1 || /^\?\?/"'       # untracke
 alias gsti='git status --porcelain -b | awk "NR==1 || /^\!\!/"'       # ignored
 alias gstz='git status --porcelain -b | awk "NR==1 || /^[MARC] /"'    # in index
 alias gsts='git status --porcelain -b | awk "NR==1 || /^[^\?\?]/"'    # not untracked
-# List aliases
+alias gstx='git_stx'
+alias gstxm='git_stx "^(M.|.M)"'    # modified
+alias gstxa='git_stx "^A[ MD]"'     # added
+alias gstxd='git_stx "^D[ M]|^ D"'  # deleted
+alias gstxr='git_stx "^R[ MD]"'     # renamed
+#alias gstxc='git_stx "^C[ MD]"'     # copied in index
+alias gstxc='git_stx "^[DAU][DAU]"' # unmerged = conflict
+alias gstxu='git_stx "^\?\?"'       # untracked = new
+alias gstxi='git_stx "^\!\!"'       # ignored
+alias gstxz='git_stx "^[MARC] "'    # in index
+alias gstxs='git_stx "^[^\?\?]"'    # not untracked# List aliases
+# List files
 alias gll='git ls-files'
 alias gls='git ls-files'
 alias glm='git ls-files -m'
