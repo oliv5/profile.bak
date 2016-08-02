9a3240089c22bb50763f5baa4cca66131a3c7f97
Oliv5 <oliv5@caramail.com>
2016-08-02 23:10:46 +0200

    [pbin] rework disk-backup.sh options.

--- pbin/disk-backup.sh
+++ pbin/disk-backup.sh
@@ -3,8 +3,9 @@ set -e # exit upon failure
 title="db"
 src="${1:?No source device specified...}"
 dst="${2:?No destination device specified...}"
-norun=""
 options=""
+norun=""
+skip=""
 shift 2
 
 # End function
@@ -42,23 +43,41 @@ echo "[$title] Mount devices"
 sudo mount -v "$src" "$mount1"
 sudo mount -v "$dst" "$mount2"
 
-# Look for the norun flag in source directory
-if [ -f "$mount1/.${title}.norun" ]; then
-	echo "[$title] Norun flag is ON"
-	norun=1
-fi
-
 # Read rsync options in source directory
 if [ -f "$mount1/.${title}.options" ]; then
 	options="$(cat "$mount1/.${title}.options")"
 	echo "[$title] Rsync options: $options"
 fi
 
+# Look for the norun flag in source directory
+if [ -f "$mount1/.${title}.norun" ]; then
+	echo "[$title] Norun flag is ON"
+	norun=1
+fi
+
+# Check source for the delete flag
+if [ -f "$mount1/.${title}.delete" ]; then
+	echo "[$title] Delete flag is ON"
+	options="$options --delete-before"
+fi
+
+# Check source for the dryrun flag
+if [ -f "$mount1/.${title}.dryrun" ]; then
+	echo "[$title] Dryrun flag is ON"
+	options="$options --dry-run"
+fi
+
+# Check source for the skip flag
+if [ -f "$mount1/.${title}.skip" ]; then
+	echo "[$title] Skip flag is ON"
+	skip=1
+fi
+
 # Main
 if [ -z "$norun" ]; then
 	# For each directory
 	for dir; do
-		if [ -d "$mount1/$dir" ]; then
+		if [ -d "$mount1/$dir" ] && ([ -z "$skip" ] || ! grep "$dir/" "$mount1/.${title}.skip"); then
 			echo "[$title] Process $mount1/$dir"
 			rsync -av "$options" -- "$mount1/$dir/" "$mount2/$dir/"
 		else
