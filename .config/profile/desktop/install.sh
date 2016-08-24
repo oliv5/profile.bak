a39aa9a87178af00186f4673ac5986e32ca368d2
Olivier Lanneluc <olanneluc@sequans.com>
2016-08-24 17:54:52 +0200

    [desktop] update source insight & xkill desktop files.

--- .config/profile/desktop/install.sh
+++ .config/profile/desktop/install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-SRCPATH="${1:-$PWD}"
-for FILE in "${SRCPATH}"/*.desktop; do
+SRC="${1:-$PWD}"
+for FILE in "${SRC}"/*.desktop; do
     ln -fsv "${FILE}" "${HOME}/.local/share/applications/$(basename "${FILE}")"
+    chmod +x "${FILE}"
 done
+for FILE in "${SRC}"/*.png "${SRC}"/*.ico; do
+    ln -fsv "${FILE}" "${HOME}/.local/share/icons/$(basename "${FILE}")"
+done
+
