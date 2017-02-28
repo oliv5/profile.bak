f3b1dca901b1f21c41c37567e932709fbd2ceae0
Olivier nexus4 <oliv5@caramail.com>
2017-02-28 19:36:39 +0100

    [rc] update psg alias on android.

--- .rc.d/android/08_sys.sh
+++ .rc.d/android/08_sys.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # Processes
-alias psg='ps -x | grep -i'
+ps -x >/dev/null 2>&1 &&
+  alias psg='ps -x | grep -i' ||
+  alias psg='ps -def | grep -i'
 alias pg='pgrep -fl'
 alias lsg='ls | grep -i'
 alias llg='ll | grep -i'
