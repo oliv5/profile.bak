fdbc5b0d8f656b99d65cc776b693decd96b765cd
Olivier Lanneluc <olanneluc@sequans.com>
2017-08-04 19:18:47 +0200

    [git] fix git_squash/git_fixup when there is nothing to commit.

--- .rc.d/12_git.sh
+++ .rc.d/12_git.sh
@@ -1013,13 +1013,13 @@ git_tag_create() {
 git_squash() {
   local COMMIT="${1:-HEAD}"
   local AHEAD="${2:-2}"
-  git commit --squash="$COMMIT" -m ""
+  git_modified && git commit --squash="$COMMIT" -m ""
   git rebase --interactive --autosquash "${COMMIT}~${AHEAD}"
 }
 git_fixup() {
   local COMMIT="${1:-HEAD}"
   local AHEAD="${2:-2}"
-  git commit --fixup="$COMMIT" -m ""
+  git_modified && git commit --fixup="$COMMIT" -m ""
   git rebase --interactive --autosquash "${COMMIT}~${AHEAD}"
 }
 
