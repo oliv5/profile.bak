a0ee0332e43b8575385b91d7bfbc2815ec8d2989
Oliv5 <oliv5@caramail.com>
2018-04-28 14:40:07 +0200

    [apt] add few commands, transform some fct in aliases.

--- .rc.d/18_apt.sh
+++ .rc.d/18_apt.sh
@@ -1,31 +1,19 @@
 #!/bin/sh
 
-# apt/dpkg commands
+# Package management (apt/dpkg)
 alias pkg_archi='dpkg --print-architecture'
 alias pkg_download='apt-get download'
 alias pkg_installed='dpkg -s'
 alias pkg_content='dpkg -L'
 alias pkg_search='dpkg -S'
 alias pkg_ls='dpkg -l'
-alias pkg_lsconf='dpkg -l | grep -E ^rc'
-alias pkg_cleanconf='dpkg -l | grep -E ^rc | xargs sudo apt-get purge'
-
-# Cleanup packages
-pkg_clean() {
-  sudo apt-get autoclean
-  sudo apt-get clean
-  sudo apt-get autoremove
-}
-
-# Make deb package from source
-deb_make() {
-  local ARCHIVE="${1:?No input archive specified}"
-  tar zxf "$ARCHIVE" "${ARCHIVE%.*}" || return 0
-  cd "${ARCHIVE%.*}"
-  ./configure || return 0
-  dh_make -s -f "../$ARCHIVE"
-  fakeroot debian/rules binary
-}
+alias pkg_ls_conf='dpkg -l | grep -E ^rc'
+alias pkg_clean='sudo apt-get autoclean; sudo apt-get clean; sudo apt-get autoremove'
+alias pkg_clean_conf='dpkg -l | grep -E ^rc | xargs sudo apt-get purge'
+alias pkg_rm_forced='sudo pkg --remove --force-remove-reinstreq'
+alias pkg_rm_ppa='sudo add-apt-repository --remove'
+alias pkg_search_old='apt-show-versions | grep "No available version"'
+alias pkg_search_old2='aptitude search "~o"'
 
 # Lock/unlock packages
 # https://askubuntu.com/questions/18654/how-to-prevent-updating-of-a-specific-package
@@ -45,15 +33,19 @@ alias apt_ls_lock='sudo apt-mark showhold'
 alias aptitude_lock='sudo aptitude hold'
 alias aptitude_unlock='sudo aptitude unhold'
 
-# Cleanup old kernels
-kernel_ls() {
-  dpkg -l 'linux-*'
-}
-kernel_current() {
-  uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/"
-}
-kernel_others() {
-  dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d'
+# Kernel management
+alias kernel_ls="dpkg -l 'linux-*'"
+alias kernel_current='uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/"'
+alias kernel_others="dpkg -l 'linux-*' | sed '/^ii/!d;/'$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d'"
+
+# Make deb package from source
+deb_make() {
+  local ARCHIVE="${1:?No input archive specified}"
+  tar zxf "$ARCHIVE" "${ARCHIVE%.*}" || return 0
+  cd "${ARCHIVE%.*}"
+  ./configure || return 0
+  dh_make -s -f "../$ARCHIVE"
+  fakeroot debian/rules binary
 }
 
 # Script to get all the PPA installed on a system
@@ -73,6 +65,3 @@ apt_ls_ppa() {
       done
   done
 }
-
-# Remove ppa
-alias apt_rm_ppa='sudo add-apt-repository --remove'
