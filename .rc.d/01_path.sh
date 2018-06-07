4f4c3658ebe1611f84510faa44e240b887b9204b
Oliv5 <oliv5@caramail.com>
2018-06-08 00:22:49 +0200

    [rc] protection building PATH when some prerequisite prgms are not there.

--- .rc.d/01_path.sh
+++ .rc.d/01_path.sh
@@ -65,6 +65,12 @@ _path_remove_absent() {
 
 # Cleanup path: remove duplicated or empty entries, expand $HOME
 _path_cleanup() {
+  str_uniq () {
+    local _IFS="${1:- }";
+    local _OFS="${2}";
+    shift 2;
+    printf '%s' "$@" | awk -vRS="$_IFS" -vORS="$_OFS" '!seen[$0]++ {str=str$1ORS} END{sub(ORS"$", "", str); printf "%s\n",str}'
+  }
   local VAR="${1:-PATH}"
   shift
   #eval export $VAR="$(echo "\$$VAR" | awk 'NF && !x[$0]++' RS='[:|\n]' ORS=':' | sed -r 's|~|'"${HOME}"'|g; s|\:\.||g; s|(^:\|:$)||')"
@@ -83,23 +89,35 @@ _path_find() {
   export $VAR="$(eval echo "\$$VAR")${RES:+:$RES}"
 }
 
+# Stub in case some prerequisites are not met
+_STUB=""
+command -v grep >/dev/null || _STUB="true"
+command -v sed >/dev/null || _STUB="true"
+command -v awk >/dev/null || _STUB="true"
+command -v find >/dev/null || _STUB="true"
+command -v stat >/dev/null || _STUB="true"
+command -v str_uniq >/dev/null || _STUB="true"
+
 # PATH aliases
-path_prepend() { _path_prepend PATH "$@"; }
-path_append() { _path_append PATH "$@"; }
-path_remove() { _path_remove PATH "$@"; }
-path_remove_fs() { _path_remove_fs PATH "$@"; }
-path_remove_absent() { _path_remove_absent PATH "$@"; }
-path_cleanup() { _path_cleanup PATH "$@"; }
-path_find() { _path_find PATH "$@"; }
-alias path_abs='readlink -f --'
+alias path_prepend="$_STUB _path_prepend PATH"
+alias path_append="$_STUB _path_append PATH"
+alias path_remove="$_STUB _path_remove PATH"
+alias path_remove_fs="$_STUB _path_remove_fs PATH"
+alias path_remove_absent="$_STUB _path_remove_absent PATH"
+alias path_cleanup="$_STUB _path_cleanup PATH"
+alias path_find="$_STUB _path_find PATH"
+alias path_abs="readlink -f --"
 
 # LD_LIBRARY_PATH aliases
 # Warning: should not use it
 # see ftp://linuxmafia.com/faq/Admin/ld-lib-path.html
-ldlibpath_prepend() { _path_prepend LD_LIBRARY_PATH "$@"; }
-ldlibpath_append() { _path_append LD_LIBRARY_PATH "$@"; }
-ldlibpath_remove() { _path_remove LD_LIBRARY_PATH "$@"; }
-ldlibpath_remove_fs() { _path_remove_fs LD_LIBRARY_PATH "$@"; }
-ldlibpath_remove_absent() { _path_remove_absent LD_LIBRARY_PATH "$@"; }
-ldlibpath_cleanup() { _path_cleanup LD_LIBRARY_PATH "$@"; }
-ldlibpath_find() { _path_find LD_LIBRARY_PATH "$@"; }
+alias ldlibpath_prepend="$_STUB _path_prepend LD_LIBRARY_PATH"
+alias ldlibpath_append="$_STUB _path_append LD_LIBRARY_PATH"
+alias ldlibpath_remove="$_STUB _path_remove LD_LIBRARY_PATH"
+alias ldlibpath_remove_fs="$_STUB _path_remove_fs LD_LIBRARY_PATH"
+alias ldlibpath_remove_absent="$_STUB _path_remove_absent LD_LIBRARY_PATH"
+alias ldlibpath_cleanup="$_STUB _path_cleanup LD_LIBRARY_PATH"
+alias ldlibpath_find="$_STUB _path_find LD_LIBRARY_PATH"
+
+# Cleanup
+unset _STUB
