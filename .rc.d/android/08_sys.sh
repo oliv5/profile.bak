74766e8e1a5736ee775547df5cc018f50994e7fb
Olivier <oliv5@caramail.com>
2018-10-26 09:49:34 +0200

    [rc] add andrpoid sys shortcuts

--- .rc.d/android/08_sys.sh
+++ .rc.d/android/08_sys.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# https://github.com/jackpal/Android-Terminal-Emulator/wiki/Android-Shell-Command-Reference
+# http://delphi.org/2013/11/installing-and-running-android-apps-from-command-line/
 
 # Sudo
 alias sudo='su root --'
@@ -11,3 +13,15 @@ alias llg='ll | grep -i'
 
 # Aliases/functions helpers
 gpp() { getprop ${@:+| grep $@}; }
+
+# Setup apps
+alias install='install -r'
+alias uninstall='pm uninstall -k'
+
+# Apps management
+start(){ am start -n "${1:?No package name specified...}/${2:-.MainActivity}"; }
+stop(){ am force-stop "${1:?No package name specified...}"; }
+
+# Services management
+start_svc(){ am startservice "${1:?No package name specified...}/${2:?No service name specified...}" ${3:+--user "$3"}; }
+stop_svc(){ am stopservice "${1:?No package name specified...}" ${3:+--user "$3"}; }
