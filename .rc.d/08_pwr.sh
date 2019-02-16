ff22aafa463e10b7cac8a57d1b2f0f4a5e461a26
Oliv5 <oliv5@caramail.com>
2019-02-16 23:53:15 +0100

    [rc] remove unecessary which statements

--- .rc.d/08_pwr.sh
+++ .rc.d/08_pwr.sh
@@ -2,20 +2,20 @@
 
 shutdown() {
   local WHEN="${1:-now}"; shift $(min 1 $#)
-  sudo $(which shutdown) -h -P "$WHEN" "$@"
+  sudo shutdown -h -P "$WHEN" "$@"
 }
 
 poweroff() {
-  sudo $(which shutdown)
+  sudo shutdown
 }
 
 reboot() {
   local WHEN="${1:-now}"; shift $(min 1 $#)
-  sudo $(which shutdown) -r "$WHEN" "$@"
+  sudo shutdown -r "$WHEN" "$@"
 }
 
 hardreboot() {
-  sudo $(which reboot) -fv "$@"
+  sudo reboot -fv "$@"
 }
 
 suspend() {
