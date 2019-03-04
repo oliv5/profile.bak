af0d253835fbab9fde7b278cc4d66743ed03d663
Oliv5 <oliv5@caramail.com>
2019-03-04 12:12:54 +0100

    [rc] txt fct uses one single sed

--- .rc.d/08_txt.sh
+++ .rc.d/08_txt.sh
@@ -1,40 +1,48 @@
 #!/bin/sh
 
 # Insert text at line(s)
+# sed -i "${MATCH}i${TEXT}" "$FILE"
 txt_insert_at() {
-	local TEXT="${1:?No text specified...}" FILE="${2:?No file specified...}"
+	local TEXT="${1:?No text specified...}" FILE="${2:?No file specified...}" SED=""
 	shift 2
-	for LINE; do
-		sed -i "${LINE}i${TEXT}" "$FILE"
+	for MATCH; do
+		SED="${SED:+$SED }-e '${MATCH}i${TEXT}'"
 	done
+	eval sed -i "$SED" "$FILE"
 }
 
 # Insert text before matching line(s)
+# sed -i -e "/$MATCH/i"$'\\\n'"$TEXT"$'\n' "$FILE"
 txt_insert_before() {
-	local TEXT="${1:?No text specified...}" FILE="${2:?No file specified...}"
+	local TEXT="${1:?No text specified...}" FILE="${2:?No file specified...}" SED=""
 	shift 2
-	for LINE; do
-		sed -i -e "/$LINE/i"$'\\\n'"$TEXT"$'\n' "$FILE"
+	for MATCH; do
+		SED="${SED:+$SED }-e '/$MATCH/i$TEXT'"
 	done
+	eval sed -i "$SED" "$FILE"
 }
 
 # Insert text after matching line(s)
+# sed -i -e "/$MATCH/a"$'\\\n'"$TEXT"$'\n' "$FILE"
 txt_insert_after() {
-	local TEXT="${1:?No text specified...}" FILE="${2:?No file specified...}"
+	local TEXT="${1:?No text specified...}" FILE="${2:?No file specified...}" SED=""
 	shift 2
-	for LINE; do
-		sed -i -e "/$LINE/a"$'\\\n'"$TEXT"$'\n' "$FILE"
+	for MATCH; do
+		SED="${SED:+$SED }-e '/$MATCH/a$TEXT'"
 	done
+	eval sed -i "$SED" "$FILE"
 }
 
 # Cut file at given lines
+# sed -i -n "${1},${2}p" "$FILE"
 txt_cut_at() {
-	local FILE="${1:?No file specified...}"
+	local FILE="${1:?No file specified...}" SED=""
 	shift
 	while [ $# -gt 1 ]; do
-		sed -i -n "${1},${2}p" "$FILE"
+		SED="${SED:+$SED }-n '${1},${2}p'"
 		shift 2
 	done
+	eval sed -i "$SED" "$FILE"
 }
 
 # Cut file before matching line
