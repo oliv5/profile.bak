c9735cda2a4048bde4b4020bd8d63d54927dd569
Oliv5 <oliv5@caramail.com>
2019-05-18 23:09:06 +0200

    [pbin] improve wol.sh

--- pbin/wol.sh
+++ pbin/wol.sh
@@ -1,32 +1,40 @@
 #!/bin/sh
+set -e
 MAC="${1?Error: please specify a MAC address}"
-ADDR="${2:-255.255.255.255}"
+IP="${2:-255.255.255.255}"
 PORT="${3:-9}"
 ITF="${4:-eth0}"
-NET="${5:-wan}"
+METHOD="$5"
+
 # Convert MAC address '-' into ':'
 MAC="$(echo $MAC | tr '-' ':')"
-# Set broadcast address
-if [ "$NET" = "lan" ] || [ "$NET" = "lan" ]; then
-  ADDR=${ADDR%.*}.255
-fi
-# Message
-echo "Usage:  wol.sh <MAC> <IP/DNS> <port[9]> <itf[eth0]>"
-echo "Config: MAC:$MAC  ADDR:$ADDR  PORT:$PORT  ITF:$ITF"
+
+# Define usable wol URLs
+wol_urls() {
+  echo "https://www.depicus.com/wake-on-lan/woli?m=${MAC}&i=${IP}&s=255.255.255.255&p=${PORT}"
+  echo "http://www.wakeonlan.me/?mobile=0&ip=${IP}:${PORT}&mac=${MAC}&pass=&schedule=&timezone=0"
+}
+
+# Log and run
+_run() {
+  echo >&2 "$@"; "$@"
+}
+
 # Execute the WOL command
-if command -v wakeonlan >/dev/null 2>&1; then
-  wakeonlan -i ${ADDR} -p ${PORT} ${MAC};
-elif command -v wol >/dev/null 2>&1; then
-  wol -i ${ADDR} -p ${PORT} ${MAC}
-elif command -v ether-wake >/dev/null 2>&1; then
-  ether-wake -i ${ITF} -b ${MAC}
-elif command -v curl >/dev/null 2>&1; then
-  URI="https://www.depicus.com/wake-on-lan/woli?m=${MAC}&i=${ADDR}&s=255.255.255.255&p=${PORT}"
-  curl "${URI}"
-elif command -v wget >/dev/null 2>&1; then
-  #URI="http://www.wakeonlan.me/?mobile=0&ip=${ADDR}:${PORT}&mac=${MAC}&pass=&schedule=&timezone=0"
-  URI="https://www.depicus.com/wake-on-lan/woli?m=${MAC}&i=${ADDR}&s=255.255.255.255&p=${PORT}"
-  wget "${URI}" -q -O /dev/null
+if [ -z "$METHOD" ] || [ "$METHOD" = "wakeonlan" ] && command -v wakeonlan >/dev/null 2>&1; then
+  _run wakeonlan -i ${IP} -p ${PORT} ${MAC};
+elif [ -z "$METHOD" ] || [ "$METHOD" = "wol" ] && command -v wol >/dev/null 2>&1; then
+  _run wol -i ${IP} -p ${PORT} ${MAC}
+elif [ -z "$METHOD" ] || [ "$METHOD" = "etherwake" ] && command -v etherwake >/dev/null 2>&1; then
+  _run etherwake -i ${ITF} -b ${MAC}
+elif [ -z "$METHOD" ] || [ "$METHOD" = "curl" ] && command -v curl >/dev/null 2>&1; then
+  for URL in $(wol_urls); do
+    _run curl -qs "${URL}" >/dev/null && break
+  done
+elif [ -z "$METHOD" ] || [ "$METHOD" = "wget" ] && command -v wget >/dev/null 2>&1; then
+  for URL in $(wol_urls); do
+    _run wget "${URL}" -q -O /dev/null && break
+  done
 else
-  echo "No appropriate WOL method available..."
+  echo >&2 "No appropriate WOL method available..."
 fi
