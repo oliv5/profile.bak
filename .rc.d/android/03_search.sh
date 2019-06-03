26aa5bf2d12ac1750097252e6a2e7e44a262693f
Olivier <oliv5@caramail.com>
2019-06-03 20:57:21 +0200

    [rc][android] fix _ffind3

--- .rc.d/android/03_search.sh
+++ .rc.d/android/03_search.sh
@@ -6,7 +6,7 @@ _ffind3() {
   local FCASE="${FCASE:--}name"
   local FILES="${1##*/}"
   local DIR="${1%"$FILES"}"
-  ${1:-true} || shift
+  [ $# -ge 1 ] && shift
   local REGEX='s/;!/" -o -not '${FCASE}' "/g ; s/&!/" -a -not '${FCASE}' "/g ; s/;/" -o '${FCASE}' "/g ; s/&/" -a '${FCASE}' /g'
   ( set -f; FILES="\"$(echo ${FILES:-*} | sed -e "$REGEX")\""
     eval find "${DIR:-.}" ${FTYPE:+-type $FTYPE} ${FILES:+$FCASE "$FILES"} ${FARGS} "$@")
