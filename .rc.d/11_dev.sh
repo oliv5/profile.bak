be2400e0561c95f4978584226b215fccbe5f8b96
Olivier Lanneluc <olanneluc@sequans.com>
2019-11-28 19:06:26 +0100

    [rc] update _dgrep3 to use git grep in all searchs (not only ref)

--- .rc.d/11_dev.sh
+++ .rc.d/11_dev.sh
@@ -21,8 +21,8 @@ alias idfflb='FCASE=-i FTYPE=l FXTYPE=l FARGS= _dfind'
 # Grep based code search
 _dgrep1()   { local ARG1="$1"; local ARG2="$2"; local ARG3="$3"; [ $# -lt 3 ] && shift $# || shift 3; (set -f; FARGS="${_DG1EXCLUDE} $@" _fgrep1 "$ARG2" "${ARG3:-.}/$ARG1"); }
 _dgrep2()   { local ARG1="$1"; local ARG2="$2"; local ARG3="$3"; [ $# -lt 3 ] && shift $# || shift 3; (set -f; _fgrep2 "$ARG2" ${_DG2EXCLUDE} "$@" "${ARG3:-.}/$ARG1"); }
-_dgrep3()   { local ARG1="$1"; local ARG2="$2"; local ARG3="$3"; [ $# -lt 3 ] && shift $# || shift 3; git grep -n ${GCASE} "$@" "$ARG2" -- "${ARG3:-.}"; }
-_dgrep()    { if git_exists "$3" && [ "$1" = "$_DGEXT_REF" ]; then _dgrep3 "$@"; else _dgrep1 "$@"; fi; }
+_dgrep3()   { local ARG1="$1"; local ARG2="$2"; local ARG3="$3"; [ $# -lt 3 ] && shift $# || shift 3; (set -f; git grep -n ${GCASE} "$@" "$ARG2" -- $(echo "$ARG1" | sed "s@^@${ARG3}*@ ; s@;@ ${ARG3}*@g")); }
+_dgrep()    { if git_exists "$3"; then _dgrep3 "$@"; else _dgrep1 "$@"; fi; }
 _DG1EXCLUDE="$_DFEXCLUDE"
 #_DG2EXCLUDE="--exclude-dir=.svn --exclude-dir=.git --exclude-dir=.repo"
 _DG2EXCLUDE="--exclude-dir=.*"
