c68add6bb3707a8f445bb15a89893867fc461afc
Olivier Lanneluc <olanneluc@sequans.com>
2020-01-24 11:48:47 +0100

    [rc] better ansi code support for scripts

--- .rc.d/00_shell.sh
+++ .rc.d/00_shell.sh
@@ -302,7 +302,7 @@ eintr() {
 ## export NC='\033[0m' # No Color
 ## echo -e "I ${RED}love${NC} Stack Overflow"
 ## printf "I ${RED}love${NC} Stack Overflow\n"
-ansi_colors() {
+ansi_export_codes() {
   export NC='\033[0m' # No Color
   export BLACK='\033[0;30m'
   export BLUE='\033[0;34m'
@@ -320,6 +320,36 @@ ansi_colors() {
   export LPURPLE='\033[1;35m'
   export YELLOW='\033[1;33m'
   export WHITE='\033[1;37m'
+}
+ansi_codes() {
+  local NC='\033[0m' # No Color
+  local BLACK='\033[0;30m'
+  local BLUE='\033[0;34m'
+  local GREEN='\033[0;32m'
+  local CYAN='\033[0;36m'
+  local RED='\033[0;31m'
+  local PURPLE='\033[0;35m'
+  local ORANGE='\033[0;33m'
+  local LGRAY='\033[0;37m'
+  local DGRAY='\033[1;30m'
+  local LBLUE='\033[1;34m'
+  local LGREEN='\033[1;32m'
+  local LCYAN='\033[1;36m'
+  local LRED='\033[1;31m'
+  local LPURPLE='\033[1;35m'
+  local YELLOW='\033[1;33m'
+  local WHITE='\033[1;37m'
+  for F; do
+    eval echo "\${$F}"
+  done
+}
+ansi_echo() {
+  local CODE="${1:-NC}"; shift
+  echo -e "$(ansi_codes "$CODE")$*$(ansi_codes "NC")"
+}
+ansi_printf() {
+  local CODE="${1:-NC}"; shift
+  printf "$(ansi_codes "$CODE")$*$(ansi_codes "NC")"
 }
 
 # Strip ANSI codes
