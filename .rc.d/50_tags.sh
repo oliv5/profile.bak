b24fa9063db12d4e8f11e1b5e1046182e87b0f1e
Oliv5 <oliv5@caramail.com>
2020-05-05 23:26:01 +0200

    [tags] generalize file scan before tag generation to skip sub-repositories; factorize options for all tag types

--- .rc.d/50_tags.sh
+++ .rc.d/50_tags.sh
@@ -3,6 +3,8 @@
 
 # Ctags settings
 _CTAGS_OPTS='--fields=+iaS --extra=+qf --c++-kinds=+p --python-kinds=-i'
+_CTAGS_REGEX='.*\.(h|c|cc|cpp|hpp|inc|py|S)$'
+_CTAGS_EXCLUDE='-not -path "*.svn*" -and -not -path "*.git" -and -not -path "/tmp/*"'
 _CTAGS_OUT='.tags'
 
 # Cscope settings
@@ -10,90 +12,84 @@ _CSCOPE_OPTS='-qbk'
 _CSCOPE_REGEX='.*\.(h|c|cc|cpp|hpp|inc|py|S)$'
 _CSCOPE_EXCLUDE='-not -path "*.svn*" -and -not -path "*.git" -and -not -path "/tmp/*"'
 _CSCOPE_OUT='.cscope.out'
-_CSCOPE_FILES='.cscope.files'
 
-# ID settings
-_ID_OUT='.id'
+# ID-utils settings
+_MKIDS_OPTS=''
+_MKIDS_REGEX='.*\.(h|c|cc|cpp|hpp|inc|py|S)$'
+_MKIDS_EXCLUDE='-not -path "*.svn*" -and -not -path "*.git" -and -not -path "/tmp/*"'
+_MKIDS_OUT='.id'
 
 # pycscope settings
+_PYCSCOPE_OPTS=''
+_PYCSCOPE_REGEX='.*\.py$'
+_PYCSCOPE_EXCLUDE='-not -path "*.svn*" -and -not -path "*.git" -and -not -path "/tmp/*"'
 _PYCSCOPE_OUT='.pycscope.out'
 
-# Make ctags
+# gtags settings
+_GTAGS_OPTS=''
+_GTAGS_REGEX='.*\.(h|c|cc|cpp|hpp|inc|py|S)$'
+_GTAGS_EXCLUDE='-not -path "*.svn*" -and -not -path "*.git" -and -not -path "/tmp/*"'
+
+# Scan recursively a directory matching a pattern
+scandir_recursive() {
+  local SRC="$(eval echo ${1:-$PWD})" # Remove tailing ~/
+  local MATCHING="${2:-.*}"
+  local EXCLUDING="${3:--not -path '*.svn*' -and -not -path '*.git' -and -not -path '/tmp/*'}"
+  find -L "$SRC" $EXCLUDING -regextype posix-egrep -regex "$MATCHING" -type f -print0 | xargs -r0 readlink -ze
+}
+
+# Scan a repository, fallback to recursive scan
+scandir_repo() {
+  local SRC="$(eval echo ${1:-$PWD})" # Remove tailing ~/
+  shift $(($#<=1?$#:1))
+  if git_exists "$SRC/.git"; then
+    git --git-dir="$SRC/.git" ls-files -z
+  elif svn_exists "$SRC"; then
+    svn ls "$SRC" | tr '\n' '\0'
+  else
+    scandir_recursive "$SRC" "$@"
+  fi
+}
+
+# Make ctags db in current repo, fallback recursive scan
 mkctags() {
   command -v >/dev/null ctags || return
   # Get directories, remove ~/
   local SRC="$(eval echo ${1:-$PWD})"
   local DST="$(eval echo ${2:-$PWD})"
+  local DB="${DST}/${_CTAGS_OUT}"
   shift $(($#<=2?$#:2))
-  # Get options
-  local CTAGS_OPTIONS="$_CTAGS_OPTS -R $@"
-  local CTAGS_DB="${DST}/${_CTAGS_OUT}"
   # Build tag file
-  #$(which ctags) $CTAGS_OPTIONS "${CTAGS_DB}" "${SRC}" 2>&1 >/dev/null | \
-  #  grep -vE 'Warning: Language ".*" already defined'
-  command ctags $CTAGS_OPTIONS -f "${CTAGS_DB}" "${SRC}"
-  ln -fs "${CTAGS_DB}" "${DST}/tags"
+  scandir_repo "$SRC" "$_CTAGS_REGEX" "$_CTAGS_EXCLUDE" |
+    xargs -r0 ctags $_CTAGS_OPTS $* -f "${DB}"
+  ln -fs "${DB}" "${DST}/tags"
 }
 
-# Scan directory and build cscope file list incrementally
-scancsdir() {
-  # Get directories, remove ~/
-  local SRC="$(eval echo ${1:-$PWD})"
-  local DST="$(eval echo ${2:-$PWD})"
-  shift $(($#<=2?$#:2))
-  # Get options
-  local CSCOPE_FILES="$DST/${_CSCOPE_FILES}"
-  # Scan directory
-  ( set -f; find -L "$SRC" $_CSCOPE_EXCLUDE -regextype posix-egrep -regex "$_CSCOPE_REGEX" -type f -execdir readlink -e "{}" \; | sed -e 's/\\/\\/g ; s/"/\"/g ; s/^/"/g ; s/$/"/g' >> "$CSCOPE_FILES" )
-}
-
-# Make cscope db from source list file (incrementally)
+# Make cscope db in current repo, fallback recursive scan
 mkcscope() {
   command -v >/dev/null cscope || return
   # Get directories, remove ~/
   local SRC="$(eval echo ${1:-$PWD})"
   local DST="$(eval echo ${2:-$PWD})"
+  local DB="$DST/${_CSCOPE_OUT}"
   shift $(($#<=2?$#:2))
-  # Get options
-  local CSCOPE_OPTIONS="$_CSCOPE_OPTS $@"
-  local CSCOPE_FILES="$DST/${_CSCOPE_FILES}"
-  local CSCOPE_DB="$DST/${_CSCOPE_OUT}"
-  # Build file list incrementally
-  scancsdir "$SRC" "$DST"
   # Build tag file
-  command cscope $CSCOPE_OPTIONS -i "$CSCOPE_FILES" -f "$CSCOPE_DB" &&
-    rm "${CSCOPE_DB}.in" "${CSCOPE_DB}.po" 2>/dev/null
+  scandir_repo "$SRC" "$_CSCOPE_REGEX" "$_CSCOPE_EXCLUDE" |
+    xargs -r0 cscope $_CSCOPE_OPTS $* -f "$DB" &&
+      rm "${DB}.in" "${DB}.po" 2>/dev/null
 }
 
-# Scan and make cscope db
-# Warning: this is not incremental
-# It erases the old database and rebuild it
-mkcscope_clean() {
-  command -v >/dev/null cscope || return
-  # Get directories, remove ~/
-  local SRC="$(eval echo ${1:-$PWD})"
-  local DST="$(eval echo ${2:-$PWD})"
-  shift $(($#<=2?$#:2))
-  # Get options
-  local CSCOPE_OPTIONS="$_CSCOPE_OPTS $@"
-  local CSCOPE_DB="$DST/${_CSCOPE_OUT}"
-  # Build tag file
-  ( set -f; find -L "$SRC" $_CSCOPE_EXCLUDE -regextype posix-egrep -regex "$_CSCOPE_REGEX" -type f -execdir readlink -f "{}" \; |\
-    command cscope $CSCOPE_OPTIONS -i '-' -f "$CSCOPE_DB" ) &&
-      rm "${CSCOPE_DB}.in" "${CSCOPE_DB}.po" 2>/dev/null
-}
-
-# Make id-utils db
+# Make id-utils db in current repo, fallback recursive scan
 mkids() {
   command -v >/dev/null mkid || return
   # Get directories, remove ~/
   local SRC="$(eval echo ${1:-$PWD})"
   local DST="$(eval echo ${2:-$PWD})"
+  local DB="$DST/${_MKIDS_OUT}"
   shift $(($#<=2?$#:2))
-  # build db
-  ( cd "$SRC"
-    command mkid -o "$DST/${_ID_OUT}"
-  )
+  # Build tag file
+  scandir_repo "$SRC" "$_MKIDS_REGEX" "$_MKIDS_EXCLUDE" |
+    xargs -r0 mkid $_MKIDS_OPTS $* -o "$DB"
 }
 
 # Make pycscope db
@@ -102,9 +98,11 @@ mkpycscope() {
   # Get directories, remove ~/
   local SRC="$(eval echo ${1:-$PWD})"
   local DST="$(eval echo ${2:-$PWD})"
+  local DB="$DST/${_PYCSCOPE_OUT}"
   shift $(($#<=2?$#:2))
   # Build tag file
-  command pycscope -R -f "$DST/${_PYCSCOPE_OUT}" "$SRC"
+  scandir_repo "$SRC" "$_PYCSCOPE_REGEX" "$_PYCSCOPE_EXCLUDE" |
+    xargs -r0 pycscope $_PYCSCOPE_OPTS $* -f "$DB"
 }
 
 # Make gtags files
@@ -114,15 +112,12 @@ mkgtags() {
   local SRC="$(eval echo ${1:-$PWD})"
   local DST="$(eval echo ${2:-$PWD})"
   shift $(($#<=2?$#:2))
-  # Build tag file
-  if [ -z "$1" ]; then
-    gtags "$DST"
-  else
-    find "$SRC" -type f -print | gtags -f - "$DST"
-  fi
+  # Build tag files
+  scandir_repo "$SRC" "$_GTAGS_REGEX" "$_GTAGS_EXCLUDE" |
+    xargs -r0 -n1 | gtags $_GTAGS_OPTS $* -f - "$DST"
 }
 
-# Make tags and cscope db
+# Make all tags
 mktags() {
   mkctags "$@"
   mkgtags "$@"
@@ -142,14 +137,14 @@ rmctags() {
 rmcscope() {
   # Get directories, remove ~/
   local DIR="$(eval echo ${1:-$PWD})"
-  rm -v "${DIR}/${_CSCOPE_OUT}"* "${DIR}/${_CSCOPE_FILES}" 2>/dev/null
+  rm -v "${DIR}/${_CSCOPE_OUT}"* 2>/dev/null
 }
 
 # Clean id-utils db
 rmids() {
   # Get directories, remove ~/
   local DIR="$(eval echo ${1:-$PWD})"
-  rm -v "${DIR}/${_ID_OUT}" 2>/dev/null
+  rm -v "${DIR}/${_MKID_OUT}" 2>/dev/null
 }
 
 # Clean pycscope db
@@ -166,7 +161,7 @@ rmgtags() {
   rm -v "${DIR}/GPATH" "${DIR}/GRTAGS" "${DIR}/GSYMS" "${DIR}/GTAGS" 2>/dev/null
 }
 
-# Clean tags and cscope db
+# Clean all tags
 rmtags() {
   rmctags "$@"
   rmgtags "$@"
@@ -212,6 +207,13 @@ mkalltags() {
         for SRC in $(cat "$TAGNAME"); do
           echo "[pycscope] add: $SRC"
           mkpycscope "$SRC" .
+        done
+      fi
+      if [ "$TAGNAME" = ".tags.path" -o "$TAGNAME" = ".gtags.path" ]; then
+        rmgtags
+        for SRC in $(cat "$TAGNAME"); do
+          echo "[gtags] add: $SRC"
+          mkgtags "$SRC" .
         done
       fi
       echo "** Done."
