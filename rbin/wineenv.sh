14e6fa72b6b6a05e4e00a64f9da6cbf08381174f
Oliv5 <oliv5@caramail.com>
2021-05-16 17:37:54 +0200

    [bin] add wineenv.sh

---
+++ rbin/wineenv.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+PREFIX="$1"
+WINEPREFIX="$2"
+if [ -d "$PREFIX" ] && [ -d "$WINEPREFIX" ]; then
+    PREFIX="$(readlink -f "$PREFIX")"
+    WINEPREFIX="$(readlink -f "$WINEPREFIX")"
+    export PATH="$PREFIX/bin:$PATH"
+    export LD_LIBRARY_PATH="$PREFIX/bin:$LD_LIBRARY_PATH"
+    export WINEPREFIX
+    echo "Setting PATH=$PATH"
+    echo "Setting LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
+    echo "Setting WINEPREFIX=$WINEPREFIX"
+fi
