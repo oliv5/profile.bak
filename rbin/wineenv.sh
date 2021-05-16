781ddd922631051b1e4bed94de1f48225182e81a
Oliv5 <oliv5@caramail.com>
2021-05-16 17:49:56 +0200

    Fix typo

--- rbin/wineenv.sh
+++ rbin/wineenv.sh
@@ -5,7 +5,7 @@ if [ -d "$PREFIX" ] && [ -d "$WINEPREFIX" ]; then
     PREFIX="$(readlink -f "$PREFIX")"
     WINEPREFIX="$(readlink -f "$WINEPREFIX")"
     export PATH="$PREFIX/bin:$PATH"
-    export LD_LIBRARY_PATH="$PREFIX/bin:$LD_LIBRARY_PATH"
+    export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
     export WINEPREFIX
     echo "Setting PATH=$PATH"
     echo "Setting LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
