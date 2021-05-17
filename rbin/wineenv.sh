ccd534f19145d4ee5d26f7fc854df3bb8de43054
Oliv5 <oliv5@caramail.com>
2021-05-17 23:15:15 +0200

    [bin] update winenv.sh

--- rbin/wineenv.sh
+++ rbin/wineenv.sh
@@ -3,10 +3,14 @@ PREFIX="$1"
 WINEPREFIX="$2"
 if [ -d "$PREFIX" ] && [ -d "$WINEPREFIX" ]; then
     PREFIX="$(readlink -f "$PREFIX")"
-    WINEPREFIX="$(readlink -f "$WINEPREFIX")"
     export PATH="$PREFIX/bin:$PATH"
     export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
-    export WINEPREFIX
+    export WINEPREFIX="$(readlink -f "$WINEPREFIX")"
+    if [ -d "$WINEPREFIX/drive_c/windows/syswow64" ]; then
+        export LD_LIBRARY_PATH="/lib:/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+    else
+        export LD_LIBRARY_PATH="/lib/i386-linux-gnu:$LD_LIBRARY_PATH"
+    fi
     echo "Setting PATH=$PATH"
     echo "Setting LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
     echo "Setting WINEPREFIX=$WINEPREFIX"
