90e03e3dc58d5c89bc402dd7245c9add38615e18
Olivier Lanneluc <olanneluc@sequans.com>
2021-06-18 17:24:38 +0200

    [rbin] add git-subrepo setup in setup-externals.sh

--- rbin/setup-externals.sh
+++ rbin/setup-externals.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 curl https://search.maven.org/remote_content?g=com.madgag&a=bfg&v=LATEST --output ~/bin/bfg.jar && chmod +x ~/bin/bfg.jar
+git clone https://github.com/ingydotnet/git-subrepo
+cat >> "${PRIVATE:-$HOME}/.rc.local.end" <<EOF
 
+# Load git-subrepo
+source /path/to/git-subrepo/.rc
+
+EOF
