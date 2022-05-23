82ab53459254c0a97b8186a7437c34a556694bad
Olivier Lanneluc <olanneluc@sequans.com>
2022-05-23 18:23:17 +0200

    [rc] Update the hook mechanism

--- .rc.d/02_cd.sh
+++ .rc.d/02_cd.sh
@@ -11,13 +11,6 @@ cd() { cda "$@"; }
 # Clean up all custom mappings
 cdu() { unalias cd cda cdb cdf 2>/dev/null || true; unset -f cd cda cdb cdf 2>/dev/null; }
 
-# Autoload local rc when listed
-cdl() {
-    if test -f "$PWD/.rc.local" && echo "$_CD_LOAD_RC" | command grep "$PWD" >/dev/null; then
-	. "$PWD/.rc.local"
-    fi
-}
-
 # Mkdir and cd
 mkcd () { mkdir -p "$@" && cd "$@"; }
 
